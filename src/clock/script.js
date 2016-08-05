f17123e46d8772cbc5d43c6977df50f13c5876fe
jh3y <jh3y@users.noreply.github.com>
2016-08-05 02:02:41 +0100

    Update

--- src/clock/script.js
+++ src/clock/script.js
@@ -1,8 +1,7 @@
 ;(function(){
-  console.info('Initiating clock');
-  const clock = document.querySelector('.clock');
+  const watch = document.querySelector('.watch');
   const d = new Date();
-  clock.setAttribute('start-seconds', d.getSeconds());
-  clock.setAttribute('start-minutes', d.getMinutes());
-  clock.setAttribute('start-hours'  , (d.getHours() > 11) ? d.getHours() - 12 : d.getHours());
+  watch.setAttribute('start-seconds', d.getSeconds());
+  watch.setAttribute('start-minutes', d.getMinutes());
+  watch.setAttribute('start-hours'  , (d.getHours() > 11) ? d.getHours() - 12 : d.getHours());
 })();
