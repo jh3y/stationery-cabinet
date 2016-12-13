e08bf3e1d5d6dd2633730b9d059f154e40fb3378
jh3y <jh3y@users.noreply.github.com>
2016-12-13 03:25:11 +0000

    Update

--- src/parallax-var/script.js
+++ src/parallax-var/script.js
@@ -1,8 +1,14 @@
-const updateX = (e) => {
-  console.info(e)
-  document.documentElement.style.setProperty('--offsetX', e.pageX)
-  document.documentElement.style.setProperty('--offsetY', e.pageY)
-  document.documentElement.style.setProperty('--clientX', window.innerWidth)
-  document.documentElement.style.setProperty('--clientY', window.innerHeight)
+const root = document.documentElement
+
+const update = (e) => {
+  if (e.acceleration && e.acceleration.x !== null) {
+    root.style.setProperty('--X', e.acceleration.x)
+    root.style.setProperty('--Y', e.acceleration.y)
+  } else {
+    root.style.setProperty('--X', (e.pageX / innerWidth) - 0.5)
+    root.style.setProperty('--Y', (e.pageY / innerHeight) - 0.5)
+  }
 }
-document.body.addEventListener('mousemove', updateX)
+
+document.body.addEventListener('mousemove', update)
+window.ondevicemotion = update
