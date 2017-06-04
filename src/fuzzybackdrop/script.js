85f8aca117233789e2b1fa6e961570c820427d39
jh3y <jh3y@users.noreply.github.com>
2017-06-04 20:09:08 +0100

    Update

---
+++ src/fuzzybackdrop/script.js
@@ -0,0 +1,59 @@
+/**
+  * Create alias for requestAnimationFrame
+*/
+const RAF = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || function (cb) { setTimeout(cb, 1000 / 60) }
+
+/**
+  * Initialize a canvas
+*/
+const HEIGHT = window.innerHeight / 1
+const WIDTH = window.innerWidth / 1
+const TOTAL = HEIGHT * WIDTH
+
+const CANVAS = document.createElement('canvas')
+CANVAS.height = HEIGHT
+CANVAS.width = WIDTH
+const CONTEXT = CANVAS.getContext('2d')
+
+/**
+  * Start rendering some stuff.
+*/
+for (let pixel = 0; pixel < TOTAL; pixel++) {
+  const X = pixel % WIDTH
+  const Y = Math.floor(pixel / WIDTH)
+  const generateColor = () => {
+    let base = Math.floor(Math.random() * 255).toString(16)
+    return `#${base}${base}${base}`
+  }
+  const color = generateColor()
+  // CONTEXT.fillStyle = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  CONTEXT.fillStyle = color
+  CONTEXT.fillRect(X, Y, 1, 1)
+}
+
+document.body.style.background = `url(${CANVAS.toDataURL()})`
+
+const update = () => {
+  document.body.style.backgroundPosition = `
+    ${Math.floor(Math.random() * WIDTH)} ${Math.floor(Math.random() * HEIGHT)}
+  `
+
+  RAF(update)
+}
+RAF(update)
+
+
+const root = document.documentElement
+
+const move = (e) => {
+  if (e.acceleration && e.acceleration.x !== null) {
+    root.style.setProperty('--translateX', e.acceleration.x)
+    root.style.setProperty('--translateY', e.acceleration.y)
+  } else {
+    root.style.setProperty('--translateX', (e.pageX / innerWidth) - 0.5)
+    root.style.setProperty('--translateY', (e.pageY / innerHeight) - 0.5)
+  }
+}
+
+document.body.addEventListener('mousemove', move)
+window.ondevicemotion = move
