ddc97c76c679708a70b91402fab4270f65f7365e
jh3y <jh3y@users.noreply.github.com>
2017-06-06 01:08:15 +0100

    Update

--- src/hypno/script.js
+++ src/hypno/script.js
@@ -7,67 +7,137 @@ const RAF = window.requestAnimationFrame || window.webkitRequestAnimationFrame |
 /**
   * Create some sizing variables
 */
-const HEIGHT = 100
-const WIDTH  = 100
-
+const DEFAULT_SIZE = 500
+const SIZE = Math.min(window.innerWidth, DEFAULT_SIZE)
+let PLAY = 150
+let X = -(PLAY / 2)
+let Y = -(PLAY / 2)
 /**
   * Initialise a canvas
 */
 const CANVAS = document.querySelector('canvas')
 CANVAS.style.background = '#111'
-CANVAS.style.height = HEIGHT
-CANVAS.style.width  = WIDTH
-CANVAS.style.borderRadius = HEIGHT / 2
-CANVAS.height       = HEIGHT
-CANVAS.width        = WIDTH
+CANVAS.style.height = SIZE
+CANVAS.style.width  = SIZE
+CANVAS.style.borderRadius = SIZE / 2
+CANVAS.height       = SIZE
+CANVAS.width        = SIZE
 
 const CONTEXT       = CANVAS.getContext('2d')
 CONTEXT.save()
 
 class Circle {
-  constructor() {
+  constructor(color = '#FFF') {
     const internalCanvas = document.createElement('canvas')
-    internalCanvas.height = HEIGHT
-    internalCanvas.width  = WIDTH
+    internalCanvas.height = SIZE + PLAY
+    internalCanvas.width  = SIZE + PLAY
     /**
       * Initialize with a starting SIZE and CANVAS reference
     */
-    this.__SIZE   = 0
-    this.__CANVAS = internalCanvas
+    this.__RENDERSIZE = SIZE + PLAY
+    this.__COLOR   = color
+    this.__SIZE    = 0
+    this.__CANVAS  = internalCanvas
+    this.__CONTEXT = internalCanvas.getContext('2d')
+    this.__ACTIVE  = true
   }
   getSize() {
     return this.__SIZE
   }
+  reset() {
+    this.__SIZE   = 0
+    this.__ACTIVE = false
+  }
   update() {
-    const context = this.__CANVAS.getContext('2d')
-    context.fillStyle = '#FFF'
-    context.arc(HEIGHT / 2, HEIGHT / 2, this.__SIZE, 0, 360)
-    context.fill()
+    this.__ACTIVE = true
+    this.__CONTEXT.clearRect(0, 0, this.__RENDERSIZE, this.__RENDERSIZE)
+    this.__CONTEXT.save()
+    this.__CONTEXT.fillStyle = this.__COLOR
+    this.__CONTEXT.beginPath()
+    this.__CONTEXT.arc(this.__RENDERSIZE / 2, this.__RENDERSIZE / 2, this.__SIZE, 0, 360)
+    this.__CONTEXT.fill()
+    this.__CONTEXT.closePath()
+    this.__CONTEXT.restore()
     this.__SIZE = this.__SIZE + 1
     return this.__CANVAS
   }
 }
 
 let circle
+let FRAME_COUNT = 0
+const generateInterval = (t) => Math.max(20, Math.floor(Math.random() * t))
+let INTERVALS = [generateInterval(100), generateInterval(20), generateInterval(35)]
+let POOL
+
+const generateColor = () => {
+  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  if (color.length !== 7) generateColor()
+  return color
+}
+
+const isUpdate = () => {
+  let update = false
+  for (const interval of INTERVALS) {
+    if (FRAME_COUNT % interval === 0) update = true
+  }
+  return update
+}
+
 const render = () => {
   /**
     * Generate a red circle and then scale it out as you go.
   */
-  CONTEXT.clearRect(0, 0, HEIGHT, HEIGHT)
-  if (!circle) circle = new Circle()
-  /**
-    * Need to try and pool the circles here. Only update until size and then
-    * release as the next makes it to size.
-  */
-  if (circle.getSize() === (HEIGHT / 2)) {
-    CONTEXT.drawImage(circle.__CANVAS, 0, 0)
-  } else {
-    CONTEXT.drawImage(circle.update(), 0, 0)
+  CONTEXT.clearRect(0, 0, SIZE, SIZE)
+  if (!POOL) POOL = []
+  // if (FRAME_COUNT % INTERVALS[0] ===  0 || FRAME_COUNT % INTERVALS[1] === 0) {
+  if (isUpdate()) {
+    const checkAvailability = () => {
+      if (POOL.length === 0) return false
+      let availability = false
+      for (const c of POOL) {
+        if (c.getSize() === 0) {
+          availability = true
+        }
+      }
+      return availability
+    }
+    /**
+      * If there are no available circles, add one.
+    */
+    const availability = checkAvailability()
+    if (!availability) {
+      const newColor = generateColor()
+      POOL.push(new Circle(newColor))
+    } else {
+      const available = POOL.shift()
+      available.__ACTIVE = true
+      POOL.push(available)
+    }
   }
-  CONTEXT.restore()
 
+  for (let c = 0; c < POOL.length; c++) {
+    const circle = POOL[c]
+    const olderSiblingCircle = POOL[c + 1]
+    if (circle.__ACTIVE && olderSiblingCircle && olderSiblingCircle.getSize() === (circle.__RENDERSIZE / 2)) {
+      circle.reset()
+    } else if (circle.__ACTIVE && circle.getSize() === (circle.__RENDERSIZE / 2)) {
+      CONTEXT.drawImage(circle.__CANVAS, X, Y)
+    } else if (circle.__ACTIVE) {
+      CONTEXT.drawImage(circle.update(), X, Y)
+    }
+  }
+
+  CONTEXT.restore()
+  FRAME_COUNT++
   RAF(render)
 }
 
 
 RAF(render)
+
+const offset = (e) => {
+  X = ((PLAY / window.innerWidth) * e.pageX) - (PLAY)
+  Y = ((PLAY / window.innerHeight) * e.pageY) - (PLAY)
+}
+
+document.addEventListener('mousemove', offset)
