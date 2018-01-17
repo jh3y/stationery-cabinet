8dfe88f13304482a7637b7554f8a0e9847934c5c
jh3y <jh3y@users.noreply.github.com>
2018-01-17 20:21:38 +0000

    Refactor and comment caret-position

--- src/caret-position/script.js
+++ src/caret-position/script.js
@@ -1,136 +1,196 @@
+// create enumeration object for repeated class names
+const CLASSES = {
+  marker: 'input__marker',
+  visible: 'input__marker--visible',
+}
 
-
-const getPositionInput = document.querySelector('.get-position-input .input')
-const getPositionTextArea = document.querySelector('.get-position-textarea .input')
-const getSelectionTextArea = document.querySelector('.get-selection-textarea .input')
-const getSelectionInput = document.querySelector('.get-selection-input .input')
-
-
-// This is the main function for grabbing the actual position for where a marker should be placed.
-// This is before taking into account any scroll position or element padding
-const getEndCoordinates = (e, selectionPoint) => {
-  // grab the element that has the input listener
-  const input = e.currentTarget
-  // get the current caret position with selectionEnd, this tells us at which index of the content we are currently at
-  const caretPosition = selectionPoint
+/**
+ * returns x, y coordinates for absolute positioning of a span within a given text input
+ * at a given selection point
+ * @param {object} input - the input element to obtain coordinates for
+ * @param {number} selectionPoint - the selection point for the input
+ */
+const getCaretXY = (input, selectionPoint) => {
   // create a dummy element that will be a clone of our input
   const div = document.createElement('div')
-  // create a marker element that will get it's position from
-  const span = document.createElement('span')
   // get the computed style of the input and clone it onto the dummy element
   const copyStyle = getComputedStyle(input)
   for (const prop of copyStyle) {
     div.style[prop] = copyStyle[prop]
   }
-  // append it to the body
+  // append the dummy element to the body
   document.body.appendChild(div)
+  // we need a character that will replace whitespace when filling our dummy element
   const swap = '.'
   // set the div content to that of the textarea up until selection
-  // console.info('value', input.value, input.value.substr(0, caretPosition).toString(), input.value.replace(/ /g, '&nbsp'))
   const inputValue = input.value.replace(/ /g, swap)
-  const textContent = inputValue.substr(0, caretPosition)
+  const textContent = inputValue.substr(0, selectionPoint)
+  // set the text content of the dummy element div
   div.textContent = textContent
   if (input.tagName === 'TEXTAREA') div.style.height = 'auto'
   // if a single line input then the div needs to be single line and not break out like a text area
   if (input.tagName === 'INPUT') div.style.width = 'auto'
-  // give the span some content so it isn't just appended to a newline
-  span.textContent = inputValue.substr(caretPosition) || swap
+  // create a marker element to obtain caret position
+  const span = document.createElement('span')
+  // set its positioning to absolute
   span.style.position = 'absolute'
   // append the span marker to the div
   div.appendChild(span)
-  // get the marker position
-  const {
-    offsetTop: top,
-    offsetLeft: left,
-  } = span
-  // remove the div from the body once the coordinates have been grabbed
+  // get the marker position, this is the caret position top and left
+  const { offsetLeft: x, offsetTop: y } = span
+  // lastly, remove that dummy element
+  // NOTE:: can comment this out for debugging purposes if you want to see where that span is rendered
   document.body.removeChild(div)
+  // return an object with the x and y of the caret
   return {
-    top,
-    left,
+    x,
+    y,
   }
 }
 
-const showPositionMarker = (e) => {
-  const marker = e.currentTarget.parentElement.querySelector('.input__marker')
-  const processClick = (evt) => {
+/**
+ * shows a position marker that highlights where the caret is
+ * @param {object} e - the input or click event that has been fired
+ */
+const showPositionMarker = e => {
+  // grab the marker for the input
+  const marker = e.currentTarget.parentElement.querySelector(
+    `.${CLASSES.marker}`
+  )
+  // create a function that will handle clicking off of the input and hide the marker
+  const processClick = evt => {
     if (e !== evt && evt.target !== e.target) {
-      marker.classList.remove('input__marker--visible')
-      marker.__IS_SHOWING = false
-      document.removeEventListener('click', processClick)
+      toggleMarker()
     }
   }
-  if (!marker.__IS_SHOWING) {
-    document.addEventListener('click', processClick)
-    marker.__IS_SHOWING = true
-    marker.classList.add('input__marker--visible')
+  // create a function that will toggle the showing of the marker
+  const toggleMarker = () => {
+    marker.__IS_SHOWING = !marker.__IS_SHOWING
+    document[
+      marker.classList.contains(CLASSES.visible)
+        ? 'removeEventListener'
+        : 'addEventListener'
+    ]('click', processClick)
+    marker.classList[
+      marker.classList.contains(CLASSES.visible) ? 'remove' : 'add'
+    ](CLASSES.visible)
   }
+  // if the marker isn't showing, show it
+  if (!marker.__IS_SHOWING) toggleMarker()
+  // if the marker is showing, update its position
   if (marker.__IS_SHOWING) {
+    // grab the input element
+    const { currentTarget: input } = e
+    // grab the properties from the input that we are interested in
     const {
-      top,
-      left,
-    } = getEndCoordinates(e, e.currentTarget.selectionEnd)
-    // grab the right padding
-    const padding = parseInt(getComputedStyle(e.currentTarget).paddingRight, 10)
-    const {
-      paddingRight,
-      paddingBottom,
-      lineHeight,
-    } = getComputedStyle(e.currentTarget)
-    marker.style.left = Math.min(left - e.currentTarget.scrollLeft, e.currentTarget.offsetWidth - padding)
-    marker.style.top = Math.min(top - e.currentTarget.scrollTop, e.currentTarget.offsetHeight - parseInt(lineHeight, 10))
+      offsetHeight,
+      offsetWidth,
+      scrollLeft,
+      scrollTop,
+      selectionEnd,
+    } = input
+    // get style property values that we are interested in
+    const { lineHeight, paddingRight } = getComputedStyle(input)
+    // get the caret X and Y from our helper function
+    const { x, y } = getCaretXY(input, selectionEnd)
+    // set the marker positioning
+    // for the left positioning we ensure that the maximum left position is the width of the input minus the right padding using Math.min
+    // we also account for current scroll position of the input
+    marker.style.left = Math.min(
+      x - scrollLeft,
+      offsetWidth - parseInt(paddingRight, 10)
+    )
+    // for the top positioning we ensure that the maximum top position is the height of the input minus line height
+    // we also account for current scroll position of the input
+    marker.style.top = Math.min(
+      y - scrollTop,
+      offsetHeight - parseInt(lineHeight, 10)
+    )
   }
 }
 
-const getSelectionArea = (e) => {
-  const marker = e.currentTarget.parentElement.querySelector('.input__marker')
+/**
+ * shows a position marker for where a user has selected input content
+ * @param {object} e - mouseup event for text selection
+ */
+const getSelectionArea = e => {
+  // grab the input element
+  const { currentTarget: input } = e
+  // grab the properties of the input we are interested in
   const {
+    offsetWidth,
+    scrollLeft,
+    scrollTop,
     selectionStart,
     selectionEnd,
-  } = e.currentTarget
-  const processClick = (evt) => {
+  } = input
+  // grab styling properties we are interested in
+  const { paddingRight } = getComputedStyle(input)
+  // grab the marker for the input
+  const marker = input.parentElement.querySelector(`.${CLASSES.marker}`)
+  // create a function that will handle clicking off of the input and hide the marker
+  const processClick = evt => {
     if (e !== evt && evt.target !== e.target) {
-      marker.classList.remove('input__marker--visible')
-      marker.__IS_SHOWING = false
-      document.removeEventListener('click', processClick)
+      toggleMarker()
     }
   }
+  // create a function that will toggle the showing of the marker
+  const toggleMarker = () => {
+    marker.__IS_SHOWING = !marker.__IS_SHOWING
+    document[
+      marker.classList.contains(CLASSES.visible)
+        ? 'removeEventListener'
+        : 'addEventListener'
+    ]('click', processClick)
+    marker.classList[
+      marker.classList.contains(CLASSES.visible) ? 'remove' : 'add'
+    ](CLASSES.visible)
+  }
+  // if selectionStart === selectionEnd then there is no actual selection, hide the marker and return
   if (selectionStart === selectionEnd) {
-    if (marker.__IS_SHOWING) {
-      marker.__IS_SHOWING = false
-      marker.classList.remove('input__marker--visible')
-      document.removeEventListener('click', processClick)
-    }
+    if (marker.__IS_SHOWING) toggleMarker()
     return
   }
-  const {
-    top: startTop,
-    left: startLeft,
-  } = getEndCoordinates(e, selectionStart)
-  const {
-    top: endTop,
-    left: endLeft,
-  } = getEndCoordinates(e, selectionEnd)
+  // we need to get the start and end positions so we can work out a midpoint to show our marker
+  // first, get the starting top and left using selectionStart
+  const { y: startTop, x: startLeft } = getCaretXY(input, selectionStart)
+  // then get the ending top and left using selectionEnd
+  const { y: endTop, x: endLeft } = getCaretXY(input, selectionEnd)
+  // if the marker isn't showing and there's a selection, show the marker
   if (!marker.__IS_SHOWING && selectionStart !== selectionEnd) {
-    document.addEventListener('click', processClick)
-    marker.__IS_SHOWING = true
-    marker.classList.add('input__marker--visible')
+    toggleMarker()
   }
+  // if the marker is showing then update its position
   if (marker.__IS_SHOWING) {
-    // startTop will always be the smallest value and we will always put the marker at the top of the selection so
-    marker.style.top = startTop - e.currentTarget.scrollTop
-    // as for the left marker
-    // if startTop doesn't equal endTop then we have a multiline selection
-    // therefore the middle will be the furthest point possible right minus the start halved and added to the start, mouthful
-    const endPoint = (startTop !== endTop) ? e.currentTarget.offsetWidth - parseInt(getComputedStyle(e.currentTarget).paddingRight, 10) : endLeft
-    const newLeft = ((endPoint - startLeft) / 2) + startLeft
-    marker.style.left = newLeft - e.currentTarget.scrollLeft
+    // we don't care about the value of endTop as our marker will always show at the top point and this will always be startTop
+    // account for scroll position by negating scrollTop
+    marker.style.top = startTop - scrollTop
+    // as for left positioning, we need to first work out if the end point is on the same line or we have multiline selection
+    // in the latter case, the endpoint will be the furthest possible right selection point
+    const endPoint =
+      startTop !== endTop ? offsetWidth - parseInt(paddingRight, 10) : endLeft
+    // we want the marker to show above the selection and in the middle of the selection so start point plus halve the endpoint minus the start point
+    const newLeft = startLeft + ((endPoint - startLeft) / 2)
+    // set the left positioning taking into account the scroll position
+    marker.style.left = newLeft - scrollLeft
   }
-
 }
+
+// grab instance of different inputs
+const getPositionInput = document.querySelector('.get-position-input .input')
+const getPositionTextArea = document.querySelector(
+  '.get-position-textarea .input'
+)
+const getSelectionTextArea = document.querySelector(
+  '.get-selection-textarea .input'
+)
+const getSelectionInput = document.querySelector('.get-selection-input .input')
+// bind event listeners to the different text inputs
 getPositionInput.addEventListener('input', showPositionMarker)
 getPositionInput.addEventListener('click', showPositionMarker)
 getPositionTextArea.addEventListener('input', showPositionMarker)
 getPositionTextArea.addEventListener('click', showPositionMarker)
 getSelectionTextArea.addEventListener('mouseup', getSelectionArea)
 getSelectionInput.addEventListener('mouseup', getSelectionArea)
+getSelectionTextArea.addEventListener('input', getSelectionArea)
+getSelectionInput.addEventListener('input', getSelectionArea)
