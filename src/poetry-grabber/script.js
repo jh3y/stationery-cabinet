e6ba03cc549b561f0462b88e45566da38256ca8a
jh3y <jh3y@users.noreply.github.com>
2018-03-16 12:10:12 +0000

    Add babel plugins and start on poetry grabber

--- src/poetry-grabber/script.js
+++ src/poetry-grabber/script.js
@@ -1,30 +1,87 @@
-const { createContext, Component } = React
+const { createContext, Component, Fragment } = React
 
 const { render } = ReactDOM
 
 const keyframes = styled.keyframes
 const styled = styled.default
 
+const prefixUrl = 'https://crossorigin.me/http://poetrydb.org'
+
+const Poem = styled.div``
+const PoemTitle = styled.h2``
+const ErrorContainer = styled.div``
+const ErrorMsg = styled.h2``
+const TryAgain = styled.button``
 class App extends Component {
   state = {
+    loading: true,
     poem: undefined,
   }
   static defaultProps = {
     stars: [],
   }
 
-  componentDidMount = () => {
-    axios.get('https://crossorigin.me/http://poetrydb.org/author,title/Shakespeare;Sonnet')
-      .then((poem) => this.setState({poem}))
+  componentDidMount() {
+    this.getPoem()
   }
 
+  getPoem = async () => {
+    this.setState({
+      error: false,
+      loading: true,
+    }, async () => {
+      try {
+        const poem = await this.getRandomPoem()
+        this.setState({
+          error: false,
+          loading: false,
+          poem,
+        })
+      } catch (e) {
+        this.setState({
+          error: true,
+          loading: false,
+        })
+      }
+    })
+  }
+
+  async getRandomPoem() {
+    // ⚠️ Triple await usage ahead 😭 😵
+    // grab a poet
+    const authors = await (await (await fetch(`${prefixUrl}/author`)).json())
+      .authors
+    const author = authors[Math.floor(Math.random() * authors.length)]
+    // get a random poem title for that poet
+    const poems = await (await fetch(
+      `${prefixUrl}/author/${author}/title`
+    )).json()
+    const chosenPoem = poems[Math.floor(Math.random() * poems.length)]
+    // grab the poet with some more pretty triple await
+    const poem = await (await (await fetch(
+      `${prefixUrl}/title/${chosenPoem.title}/title,author,lines`
+    )).json())[0]
+    return poem
+  }
   render = () => {
-    const {
-      poem
-    } = this.state
-    console.info(poem)
+    const { getPoem, state } = this
+    const { error, loading, poem } = state
+    console.info(loading, error, poem)
     return (
-      <h1>Hey there!</h1>
+      <Fragment>
+        {loading && <h2>Currently talking to the poets...</h2>}
+        {error && (
+          <ErrorContainer>
+            <ErrorMsg>Oops, look like something went wrong, please try again</ErrorMsg>
+            <TryAgain onClick={getPoem}>Try again</TryAgain>
+          </ErrorContainer>
+        )}
+        {poem && (
+          <Poem>
+            <PoemTitle>{poem.title}</PoemTitle>
+          </Poem>
+        )}
+      </Fragment>
     )
   }
 }
