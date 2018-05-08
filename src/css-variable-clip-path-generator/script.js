ae5e6475909a64031e63ba52f83309f91fab1188
jh3y <jh3y@users.noreply.github.com>
2018-05-09 00:39:33 +0100

    Finish up react clip-path generator :tada:

--- src/css-variable-clip-path-generator/script.js
+++ src/css-variable-clip-path-generator/script.js
@@ -2,59 +2,59 @@ const { Component, Fragment } = React
 const { render } = ReactDOM
 
 const getClipPath = (nodes, size, mode) => {
+  let path = ''
   let centerX
   let centerY
-  let path = ''
+  let dragX
+  let dragY
+  const getRatio = d => Math.floor(d / size * 100)
+  if (!nodes.length) return null
   switch (mode) {
     case MODES.POLYGON:
       for (let n = 0; n < nodes.length; n++) {
-        const node = nodes[n]
-        path += `${Math.floor(node.x / size * 100)}% ${Math.floor(
-          node.y / size * 100
-        )}%${n === nodes.length - 1 ? '' : `, `}`
+        const { x, y } = nodes[n]
+        path += `${getRatio(x)}% ${getRatio(y)}%${
+          n === nodes.length - 1 ? '' : `, `
+        }`
       }
       break
     case MODES.INSET:
-      // iterate through nodes TRBL
       for (let n = 0; n < nodes.length; n++) {
+        const { x, y } = nodes[n]
         switch (n) {
           case 0:
-            path += `${Math.floor(nodes[n].y / size * 100)}% `
+            path += `${getRatio(y)}% `
             break
           case 1:
-            path += `${100 - Math.floor(nodes[n].x / size * 100)}% `
+            path += `${100 - getRatio(x)}% `
             break
           case 2:
-            path += `${100 - Math.floor(nodes[n].y / size * 100)}% `
+            path += `${100 - getRatio(y)}% `
             break
           case 3:
-            path += `${Math.floor(nodes[n].x / size * 100)}%`
+            path += `${getRatio(x)}%`
             break
         }
       }
       break
     case MODES.ELLIPSE:
-      const xDistance = Math.abs(
-        Math.floor((nodes[1].x - nodes[0].x) / size * 100)
-      )
-      const yDistance = Math.abs(
-        Math.floor((nodes[2].y - nodes[0].y) / size * 100)
-      )
-      path = `${xDistance}% ${yDistance}% at ${Math.floor(
-        nodes[0].x / size * 100
-      )}% ${Math.floor(nodes[0].y / size * 100)}%`
+      ;({ x: centerX, y: centerY } = nodes[0])
+      dragX = nodes[1].x
+      dragY = nodes[2].y
+      const xDistance = Math.abs(getRatio(dragX - centerX))
+      const yDistance = Math.abs(getRatio(dragY - centerY))
+      path = `${xDistance}% ${yDistance}% at ${getRatio(centerX)}% ${getRatio(
+        centerY
+      )}%`
       break
     case MODES.CIRCLE:
-      const { x: centerX, y: centerY } = nodes[0]
-      const { x, y } = nodes[1]
-      const distY = y - centerY
-      const distX = x - centerX
-      const distance = Math.floor(
-        Math.sqrt(distX * distX + distY * distY) / size * 100
-      )
-      path = `${distance}% at ${Math.floor(centerX / size * 100)}% ${Math.floor(
-        centerY / size * 100
-      )}%`
+      ;({ x: centerX, y: centerY } = nodes[0])
+      dragX = nodes[1].x
+      dragY = nodes[1].y
+      const distX = dragX - centerX
+      const distY = dragY - centerY
+      const distance = getRatio(Math.sqrt(distX * distX + distY * distY))
+      path = `${distance}% at ${getRatio(centerX)}% ${getRatio(centerY)}%`
       break
     default:
       path = 'No mode selected'
@@ -69,10 +69,10 @@ class ClipPathNode extends Component {
     y: this.props.y,
   }
   componentDidMount = () => {
-    const { el } = this
-    el.addEventListener('mousedown', this.startMove)
-    el.addEventListener('touchstart', this.startMove)
-    el.addEventListener('click', this.removeNode)
+    const { el, startMove, removeNode } = this
+    el.addEventListener('mousedown', startMove)
+    el.addEventListener('touchstart', startMove)
+    el.addEventListener('click', removeNode)
   }
   removeNode = () => {
     const { id, removing, onRemove } = this.props
@@ -80,18 +80,19 @@ class ClipPathNode extends Component {
   }
   getPageXY = e => {
     let { pageX: x, pageY: y, touches } = e
-    // If a touch move then use the touches property 👍
     if (touches && touches.length === 1) {
       x = touches[0].pageX
       y = touches[0].pageY
     }
+    y -= window.pageYOffset
     return {
       x,
       y,
     }
   }
   startMove = e => {
-    if (this.props.removing) return
+    const { onMove, removing, restrictX, restrictY } = this.props
+    if (removing) return
     const { x, y } = this.getPageXY(e)
     const move = e => {
       const { x: oldX, y: oldY } = this.state
@@ -106,10 +107,10 @@ class ClipPathNode extends Component {
       const y = Math.min(Math.max(0, pageY - top), height)
       this.setState(
         {
-          x: this.props.restrictX ? oldX : x,
-          y: this.props.restrictY ? oldY : y,
+          x: restrictX ? oldX : x,
+          y: restrictY ? oldY : y,
         },
-        () => this.props.onMove(this)
+        () => onMove(this)
       )
     }
     const endMove = () => {
@@ -118,7 +119,7 @@ class ClipPathNode extends Component {
           moving: false,
         },
         () => {
-          this.props.onMove(this)
+          onMove(this)
           document.body.removeEventListener('mousemove', move)
           document.body.removeEventListener('mouseup', endMove)
           if (e.touches && e.touches.length === 1) {
@@ -174,58 +175,102 @@ const SIZE = 300
 const PRESETS = {
   CIRCLE: [
     {
-      x: SIZE / 2,
-      y: SIZE / 2,
+      x: 195,
+      y: 105,
     },
     {
-      x: 250,
-      y: 250,
+      x: 140,
+      y: 50,
     },
   ],
   ELLIPSE: [
     {
-      x: SIZE / 2,
-      y: SIZE / 2,
+      x: 195,
+      y: 150,
     },
     {
-      x: 200,
-      y: SIZE / 2,
+      x: 285,
+      y: 150,
     },
     {
-      x: SIZE / 2,
-      y: 25,
+      x: 195,
+      y: 30,
     },
   ],
   INSET: [
     {
-      x: SIZE / 2,
-      y: 25,
+      x: 195,
+      y: 30,
     },
     {
-      x: 275,
-      y: SIZE / 2,
+      x: 270,
+      y: 165,
     },
     {
-      x: SIZE / 2,
-      y: 275,
+      x: 195,
+      y: 300,
     },
     {
-      x: 25,
-      y: SIZE / 2,
+      x: 120,
+      y: 165,
     },
   ],
   POLYGON: [
     {
-      x: 40,
-      y: 100,
+      x: 255,
+      y: 20,
     },
     {
-      x: 75,
-      y: 120,
+      x: 210,
+      y: 45,
+    },
+    {
+      x: 90,
+      y: 45,
+    },
+    {
+      x: 45,
+      y: 15,
     },
     {
       x: 0,
-      y: 45,
+      y: 75,
+    },
+    {
+      x: 30,
+      y: 135,
+    },
+    {
+      x: 30,
+      y: 225,
+    },
+    {
+      x: 75,
+      y: 270,
+    },
+    {
+      x: 120,
+      y: 285,
+    },
+    {
+      x: 180,
+      y: 285,
+    },
+    {
+      x: 225,
+      y: 275,
+    },
+    {
+      x: 270,
+      y: 225,
+    },
+    {
+      x: 270,
+      y: 135,
+    },
+    {
+      x: 300,
+      y: 75,
     },
   ],
 }
@@ -243,51 +288,59 @@ class ClipPathGenerator extends Component {
     nodes: PRESETS[MODES.POLYGON],
   }
   onUpdate = n => {
-    const nodes = this.state.nodes.map((o, idx) => {
-      if (idx === n.props.id) {
+    const {id: updatedId} = n.props
+    const {x: updatedX, y: updatedY} = n.state
+    const { mode, nodes: currentNodes } = this.state
+    const getHandlePoint = (node, axis) => {
+      const movement = n.state[axis] - currentNodes[0][axis]
+      let newPoint = node[axis] + movement
+      if (newPoint > SIZE || newPoint < 0) {
+        newPoint = currentNodes[0][axis] + currentNodes[0][axis] - newPoint
+      }
+      return Math.min(SIZE, Math.max(0, newPoint))
+    }
+    // Create a new set of nodes by mapping through the current node set
+    const nodes = currentNodes.map((o, idx) => {
+      // If it's the node that's being moved, just update it with new values
+      if (idx === updatedId) {
         o = Object.assign({}, o, {
-          x: n.state.x,
-          y: n.state.y,
+          x: updatedX,
+          y: updatedY,
         })
-      } else if (this.state.mode === MODES.ELLIPSE && n.props.id === 0) {
-        if (idx === 1) {
-          const xMove = n.state.x - this.state.nodes[0].x
-          let newX = o.x + xMove
-          if (newX > 300 || newX < 0) {
-            newX = this.state.nodes[0].x + this.state.nodes[0].x - newX
-          }
+      }
+      // Else if in INSET mode update the corresponding axis so the nodes are center aligned
+      else if (mode === MODES.INSET) {
+        // Need to update the opposite axis to still be centrally aligned in the clip
+
+        const getCenterPoint = (start, end, axis) =>
+          currentNodes[start][axis] +
+          (currentNodes[end][axis] - currentNodes[start][axis]) / 2
+        // UPDATING X
+        if (updatedId % 2 && !(idx % 2)) {
           o = Object.assign({}, o, {
-            y: n.state.y,
-            x: Math.min(300, Math.max(0, newX)),
+            x:
+              updatedId === 1
+                ? getCenterPoint(3, 1, 'x')
+                : getCenterPoint(1, 3, 'x'),
           })
-        } else if (idx === 2) {
-          const yMove = n.state.y - this.state.nodes[0].y
-          let newY = o.y + yMove
-          if (newY > 300 || newY < 0) {
-            newY = this.state.nodes[0].y + this.state.nodes[0].y - newY
-          }
+        } else if (!(updatedId % 2) && idx % 2) {
           o = Object.assign({}, o, {
-            x: n.state.x,
-            y: Math.min(300, Math.max(0, newY)),
-          })
-        }
-      } else if (this.state.mode === MODES.CIRCLE && n.props.id === 0) {
-        if (idx === 1) {
-          const xMove = n.state.x - this.state.nodes[0].x
-          let newX = o.x + xMove
-          if (newX > 300 || newX < 0) {
-            newX = this.state.nodes[0].x + this.state.nodes[0].x - newX
-          }
-          const yMove = n.state.y - this.state.nodes[0].y
-          let newY = o.y + yMove
-          if (newY > 300 || newY < 0) {
-            newY = this.state.nodes[0].y + this.state.nodes[0].y - newY
-          }
-          o = Object.assign({}, o, {
-            x: Math.min(300, Math.max(0, newX)),
-            y: Math.min(300, Math.max(0, newY)),
+            y:
+              updatedId === 2
+                ? getCenterPoint(0, 2, 'y')
+                : getCenterPoint(2, 0, 'y'),
           })
         }
+      } else if (mode === MODES.ELLIPSE && updatedId === 0 && idx !== 0) {
+        o = Object.assign({}, o, {
+          y: idx === 1 ? updatedY : getHandlePoint(o, 'y'),
+          x: idx === 1 ? getHandlePoint(o, 'x') : updatedX,
+        })
+      } else if (mode === MODES.CIRCLE && updatedId === 0 && idx !== 0) {
+        o = Object.assign({}, o, {
+          x: getHandlePoint(o, 'x'),
+          y: getHandlePoint(o, 'y'),
+        })
       }
       return o
     })
@@ -302,8 +355,8 @@ class ClipPathGenerator extends Component {
       nodes: [
         ...this.state.nodes,
         {
-          x: 150,
-          y: 150,
+          x: SIZE / 2,
+          y: SIZE / 2,
         },
       ],
     })
@@ -328,7 +381,6 @@ class ClipPathGenerator extends Component {
     })
   }
   copy = () => {
-    const path = this.path
     if (this.path) {
       this.path.select()
       document.execCommand('Copy')
@@ -337,8 +389,21 @@ class ClipPathGenerator extends Component {
       })
     }
   }
+  wipeNodes = () => {
+    this.setState({
+      nodes: []
+    })
+  }
   render = () => {
-    const { addNode, switchMode, onUpdate, removeNode, removeNodes } = this
+    const {
+      addNode,
+      copy,
+      switchMode,
+      onUpdate,
+      removeNode,
+      removeNodes,
+      wipeNodes,
+    } = this
     const { copied, mode, nodes, removing, size } = this.state
     const clipPath = getClipPath(nodes, size, mode)
     return (
@@ -348,7 +413,7 @@ class ClipPathGenerator extends Component {
             <div
               className="clip-path-generator__clipped"
               style={{
-                clipPath,
+                '--path': clipPath,
               }}
             />
             {nodes.map((n, idx) => (
@@ -377,14 +442,17 @@ class ClipPathGenerator extends Component {
           </div>
         </div>
         <div className="clip-path">
-          <div className="clip-path__value">{`clip-path: ${clipPath};`}</div>
+          <div className="clip-path__value">{clipPath ? `clip-path: ${clipPath};` : 'No clip path set'}</div>
           <input
             className="clip-path__input"
             readOnly
             ref={p => (this.path = p)}
             value={`clip-path: ${clipPath};`}
           />
-          <button className="clip-path__copy button" onClick={this.copy}>
+          <button
+            disabled={removing || !clipPath}
+            className="clip-path__copy button"
+            onClick={copy}>
             {copied ? 'Copied 👍' : 'Copy'}
           </button>
         </div>
@@ -392,6 +460,7 @@ class ClipPathGenerator extends Component {
           {Object.keys(MODES).map((m, idx) => (
             <div key={`clip-path-option--${idx}`} className="clip-path-option">
               <input
+                disabled={removing}
                 className="clip-path-option__input"
                 type="radio"
                 name="shape"
@@ -400,7 +469,9 @@ class ClipPathGenerator extends Component {
                 onChange={switchMode}
                 checked={m === mode}
               />
-              <label className='clip-path-option__label' htmlFor={`clip-path-option--${m}`}>
+              <label
+                className="clip-path-option__label"
+                htmlFor={`clip-path-option--${m}`}>
                 <div className="clip-path-option__check" />
                 {`${m.charAt(0)}${m.toLowerCase().substr(1)}`}
               </label>
@@ -409,11 +480,24 @@ class ClipPathGenerator extends Component {
         </div>
         {mode === MODES.POLYGON && (
           <div className="polygon-actions">
-            <button className="polygon-action polygon-action--add button" disabled={removing} onClick={addNode}>
+            <button
+              className="polygon-action polygon-action--add button"
+              disabled={removing}
+              onClick={addNode}>
               Add Node
             </button>
-            <button className="polygon-action--remove polygon-action button" onClick={removeNodes}>
+            <button
+              className={`polygon-action ${
+                removing ? 'polygon-action--removing' : 'polygon-action--remove'
+              } button`}
+              onClick={removeNodes}>
               {removing ? 'Done' : 'Remove Node'}
+            </button>
+            <button
+              disabled={removing}
+              className='polygon-action polygon-action--wipe button'
+              onClick={wipeNodes}>
+              {'Wipe Nodes'}
             </button>
           </div>
         )}
