5e04f0d1d89b804c93b2fbd1f79badb14ba2cc1b
jh3y <jh3y@users.noreply.github.com>
2018-05-13 04:21:06 +0100

    Finish implementation of dont feel so good bear

--- src/dont-feel-good/script.js
+++ src/dont-feel-good/script.js
@@ -1,3 +1,31 @@
-;(function(){
-  console.info('New pen created!');
-})();
+const bearGrid = document.querySelector('.bear-grid')
+const bearCells = bearGrid.querySelectorAll('.bear-grid__cell')
+const lastCell = bearCells[bearCells.length - 1]
+const replay = document.querySelector('.replay')
+
+
+const imageHasLoaded = (src) => new Promise((resolve, reject) => {
+  const img = new Image()
+  img.onload = resolve
+  img.onerror = reject
+  img.src = src
+})
+
+const showReset = () => {
+  bearGrid.classList.toggle('bear-grid--fading')
+  replay.classList.toggle('replay--show')
+}
+
+const fade = () => {
+  replay.classList.toggle('replay--show')
+  bearGrid.classList.toggle('bear-grid--fading')
+}
+
+
+lastCell.addEventListener('animationend', showReset)
+replay.addEventListener('click', fade)
+
+imageHasLoaded('https://raw.github.com/jh3y/doodles/master/dontfeelgoodbear.svg?sanitize=true')
+  .then(() => {
+    bearGrid.classList.toggle('bear-grid--fading')
+  })
