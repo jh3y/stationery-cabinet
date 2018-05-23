1f2a67d7bcbe2d292f7ab144fc8d1ae943df0edf
jh3y <jh3y@users.noreply.github.com>
2018-05-23 01:03:27 +0100

    Implement basic line item addition

--- src/cash-register-superscript/script.js
+++ src/cash-register-superscript/script.js
@@ -1,3 +1,22 @@
-;(function(){
-  console.info('New pen created!');
-})();
+const getRandom = () => Math.floor(Math.random() * 10)
+/**
+ * Vue cash register app
+ */
+const app = new Vue({
+  el: '.cash-register',
+  data: {
+    lines: 0,
+    amount: [0, 0, 0, 0],
+    temp: [0, 0, 0, 0],
+  },
+  methods: {
+    addToLine: function(e) {
+      this.temp[e.target.getAttribute('data-amount-index')] = getRandom()
+    },
+    addLine: function(e) {
+      this.amount = this.temp
+      this.temp = [0, 0, 0, 0]
+      this.lines = (this.lines + 1) % 10
+    }
+  },
+})
