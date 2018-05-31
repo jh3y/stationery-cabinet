cbacf8fe16bb4d1366e7a290136a9f17f6c74819
jh3y <jh3y@users.noreply.github.com>
2018-05-31 23:55:36 +0100

    Updates to phonebook UI concept

--- src/phonebook-details/script.js
+++ src/phonebook-details/script.js
@@ -1,26 +1,4 @@
-// const phonebook = document.querySelector('.phonebook')
-// const contacts = document.querySelectorAll('.phonebook__contact')
-// const update = () => {
-//   const contactIndex = Math.floor(phonebook.scrollTop / 50)
-//   if (contacts[contactIndex]) {
-//     const active = document.querySelector('.phonebook__contact--active')
-//     const newActive = contacts[contactIndex]
-//     const next = document.querySelector('.phonebook__contact--next')
-//     const prev = document.querySelector('.phonebook__contact--prev')
-//     if (newActive.nextElementSibling) {
-//       if (next) next.classList.remove('phonebook__contact--next')
-//       newActive.nextElementSibling.classList.add('phonebook__contact--next')
-//     }
-//     if (newActive.previousElementSibling) {
-//       if (prev) prev.classList.remove('phonebook__contact--prev')
-//       newActive.previousElementSibling.classList.add('phonebook__contact--prev')
-//     }
-//     active.classList.remove('phonebook__contact--active')
-//     newActive.classList.add('phonebook__contact--active')
-//   }
-// }
-
-// phonebook.addEventListener('scroll', update)
+TweenMax.defaultEase = Linear.easeNone
 const contacts = []
 for (let c = 0; c < 120; c++) {
   contacts.push({
@@ -28,23 +6,105 @@ for (let c = 0; c < 120; c++) {
     avatar: faker.image.avatar()
   })
 }
+const DURATION = .25
 const app = new Vue({
   el: '.phonebook',
   data: {
     contacts,
+    fakePosOne: undefined,
+    fakePosTwo: undefined,
+    messageTop: 0,
+    detailOpened: false,
+    openingDetail: false,
     updating: false,
     active: 0,
     next: 1,
     prev: undefined,
+    activeContact: undefined,
   },
   methods: {
-    handleScroll: function(e) {
-      const contactIndex = Math.floor(this.$el.scrollTop / 50)
+    updateActiveContact: function(contactIndex) {
       if (this.contacts[contactIndex]) {
         this.active = contactIndex
         this.next = contactIndex + 1
         this.prev = contactIndex - 1
       }
+    },
+    selectContact: function(contactIndex, contact) {
+      this.updateActiveContact(contactIndex)
+      this.$refs.contacts.scrollTop = (contactIndex * 50)
+      this.openingDetail = true
+      this.detailOpened = true
+      this.activeContact = contact
+      this.$nextTick(() => {
+        const el = this.$refs.contacts.children[contactIndex]
+        const list = this.$refs.contacts
+        this.messageTop = el.offsetTop - list.scrollTop
+        this.fakePosTwo =
+          el.offsetTop + el.offsetHeight - list.scrollTop
+        this.fakePosOne = list.offsetHeight - el.offsetTop + list.scrollTop
+        const openTL = new TimelineMax()
+        openTL
+          .to(this.$refs.fakeHeader, DURATION, {
+            y: -(this.fakePosOne)
+          }, 0)
+          .to(this.$refs.topFake, DURATION, {
+            bottom: 480,
+          }, 0)
+          .to(this.$refs.bottomFake, DURATION, {
+            top: 480,
+          }, 0)
+          .to(this.$refs.activeAvatar, DURATION, {
+            y: (this.messageTop / 2) - 50,
+            x: (320 / 2) - 50
+          }, 0)
+          .to(this.$refs.activeName, DURATION, {
+            paddingLeft: 0,
+            fontSize: '2rem',
+            y: (this.messageTop / 2) + 50,
+            lineHeight: '2.5rem',
+          }, 0)
+          .to(this.$refs.activeDetails, DURATION, {
+            opacity: 1,
+            y: 0
+          }, 0)
+      })
+    },
+    closeDetails: function(e) {
+      const closeTL = new TimelineMax({
+        onComplete: () => {
+          this.detailOpened = false
+          this.openingDetail = false
+        }
+      })
+      closeTL
+        .to(this.$refs.fakeHeader, DURATION, {
+          y: 0,
+        }, 0)
+        .to(this.$refs.topFake, DURATION, {
+          bottom: this.fakePosOne,
+        }, 0)
+        .to(this.$refs.bottomFake, DURATION, {
+          top: this.fakePosTwo,
+        }, 0)
+        .to(this.$refs.activeAvatar, DURATION, {
+          x: 0,
+          y: this.messageTop,
+        }, 0)
+        .to(this.$refs.activeName, DURATION, {
+          paddingLeft: 100,
+          y: this.messageTop,
+          fontSize: '1rem',
+          lineHeight: '100px',
+        }, 0)
+        .to(this.$refs.activeDetails, DURATION, {
+          opacity: 0,
+          y: 100,
+        }, 0)
+    },
+    handleScroll: function(e) {
+      const contactIndex = Math.floor(this.$refs.contacts.scrollTop / 50)
+      this.updateActiveContact(contactIndex)
       this.updating = false
     },
     update: function(e) {
@@ -57,6 +117,6 @@ const app = new Vue({
   },
   mounted: function() {
     this.$el.classList.remove('phonebook--loading')
-    this.$el.addEventListener('scroll', this.update)
+    this.$refs.contacts.addEventListener('scroll', this.update)
   }
 })
