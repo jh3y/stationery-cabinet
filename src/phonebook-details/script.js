68ffc3400dbffa60761ee9c4a407ada173fecadd
jh3y <jh3y@users.noreply.github.com>
2018-06-01 18:49:31 +0100

    Finish implementation of contact list concept

--- src/phonebook-details/script.js
+++ src/phonebook-details/script.js
@@ -1,12 +1,41 @@
 TweenMax.defaultEase = Linear.easeNone
-const contacts = []
-for (let c = 0; c < 120; c++) {
-  contacts.push({
-    name: faker.name.findName(),
-    avatar: faker.image.avatar()
-  })
+let contacts = []
+for (let c = 0; c < 100; c++) {
+  const newContact = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    avatar: faker.internet.avatar(),
+    number: faker.phone.phoneNumber(),
+  }
+  const items = [
+    { email: 'internet.email' },
+    { homenumber: 'phone.phoneNumber' },
+    { twitter: 'internet.userName' },
+    { instagram: 'internet.userName' },
+    { codepen: 'internet.userName' },
+  ]
+  const add = i => {
+    const key = Object.keys(i)[0]
+    const value = i[key].split('.')
+    if (Math.random() > 0.5)
+      newContact[key] = faker[value[0]][value[1]](
+        value[1] === 'phoneNumber' ? null : newContact.firstName,
+        newContact.lastName
+      )
+  }
+  for (let i of items) add(i)
+  if (Math.random() > 0.5) newContact.whatsapp = newContact.number
+  contacts.push(newContact)
 }
-const DURATION = .25
+contacts = contacts.sort((a, b) => (a.firstName > b.firstName ? 1 : -1))
+
+const DURATION = 0.25
+const CLASSES = {
+  LOADING: 'phonebook--loading',
+}
+const CONTACTHEIGHT = 50
+const PADDING = 20
+
 const app = new Vue({
   el: '.phonebook',
   data: {
@@ -15,12 +44,11 @@ const app = new Vue({
     fakePosTwo: undefined,
     messageTop: 0,
     detailOpened: false,
-    openingDetail: false,
     updating: false,
     active: 0,
     next: 1,
     prev: undefined,
-    activeContact: undefined,
+    activeContact: contacts[0],
   },
   methods: {
     updateActiveContact: function(contactIndex) {
@@ -28,95 +56,175 @@ const app = new Vue({
         this.active = contactIndex
         this.next = contactIndex + 1
         this.prev = contactIndex - 1
+        this.activeContact = this.contacts[contactIndex]
       }
     },
     selectContact: function(contactIndex, contact) {
       this.updateActiveContact(contactIndex)
-      this.$refs.contacts.scrollTop = (contactIndex * 50)
-      this.openingDetail = true
+      this.$refs.contacts.scrollTop = contactIndex * CONTACTHEIGHT
       this.detailOpened = true
       this.activeContact = contact
       this.$nextTick(() => {
         const el = this.$refs.contacts.children[contactIndex]
         const list = this.$refs.contacts
+        const elStyle = el.getBoundingClientRect()
+        const listStyle = list.getBoundingClientRect()
         this.messageTop = el.offsetTop - list.scrollTop
-        this.fakePosTwo =
-          el.offsetTop + el.offsetHeight - list.scrollTop
+        this.fakePosTwo = el.offsetTop + el.offsetHeight - list.scrollTop
         this.fakePosOne = list.offsetHeight - el.offsetTop + list.scrollTop
         const openTL = new TimelineMax()
         openTL
-          .to(this.$refs.fakeHeader, DURATION, {
-            y: -(this.fakePosOne)
-          }, 0)
-          .to(this.$refs.topFake, DURATION, {
-            bottom: 480,
-          }, 0)
-          .to(this.$refs.bottomFake, DURATION, {
-            top: 480,
-          }, 0)
-          .to(this.$refs.activeAvatar, DURATION, {
-            y: (this.messageTop / 2) - 50,
-            x: (320 / 2) - 50
-          }, 0)
-          .to(this.$refs.activeName, DURATION, {
-            paddingLeft: 0,
-            fontSize: '2rem',
-            y: (this.messageTop / 2) + 50,
-            lineHeight: '2.5rem',
-          }, 0)
-          .to(this.$refs.activeDetails, DURATION, {
-            opacity: 1,
-            y: 0
-          }, 0)
+          .to(
+            this.$refs.fakeHeader,
+            DURATION,
+            {
+              y: -this.fakePosOne,
+            },
+            0
+          )
+          .to(
+            this.$refs.topFake,
+            DURATION,
+            {
+              bottom: listStyle.height,
+            },
+            0
+          )
+          .to(
+            this.$refs.bottomFake,
+            DURATION,
+            {
+              top: listStyle.height,
+            },
+            0
+          )
+          .to(
+            this.$refs.activeAvatar,
+            DURATION,
+            {
+              y: this.messageTop / 2 - elStyle.height / 2,
+              x: listStyle.width / 2 - elStyle.height / 2,
+            },
+            0
+          )
+          .to(
+            this.$refs.activeAvatarBack,
+            DURATION,
+            {
+              opacity: 0.65,
+            },
+            0
+          )
+          .to(
+            this.$refs.activeName,
+            DURATION,
+            {
+              paddingLeft: PADDING,
+              fontSize: '2rem',
+              y: this.messageTop / 2 + elStyle.height / 2,
+              lineHeight: '2.5rem',
+            },
+            0
+          )
+          .to(
+            this.$refs.activeDetails,
+            DURATION,
+            {
+              opacity: 1,
+              y: 0,
+            },
+            0
+          )
       })
     },
     closeDetails: function(e) {
+      const avatarStyle = this.$refs.activeAvatar.getBoundingClientRect()
       const closeTL = new TimelineMax({
         onComplete: () => {
           this.detailOpened = false
-          this.openingDetail = false
-        }
+        },
       })
       closeTL
-        .to(this.$refs.fakeHeader, DURATION, {
-          y: 0,
-        }, 0)
-        .to(this.$refs.topFake, DURATION, {
-          bottom: this.fakePosOne,
-        }, 0)
-        .to(this.$refs.bottomFake, DURATION, {
-          top: this.fakePosTwo,
-        }, 0)
-        .to(this.$refs.activeAvatar, DURATION, {
-          x: 0,
-          y: this.messageTop,
-        }, 0)
-        .to(this.$refs.activeName, DURATION, {
-          paddingLeft: 100,
-          y: this.messageTop,
-          fontSize: '1rem',
-          lineHeight: '100px',
-        }, 0)
-        .to(this.$refs.activeDetails, DURATION, {
-          opacity: 0,
-          y: 100,
-        }, 0)
+        .to(
+          this.$refs.fakeHeader,
+          DURATION,
+          {
+            y: 0,
+          },
+          0
+        )
+        .to(
+          this.$refs.topFake,
+          DURATION,
+          {
+            bottom: this.fakePosOne,
+          },
+          0
+        )
+        .to(
+          this.$refs.bottomFake,
+          DURATION,
+          {
+            top: this.fakePosTwo,
+          },
+          0
+        )
+        .to(
+          this.$refs.activeAvatar,
+          DURATION,
+          {
+            x: 0,
+            y: this.messageTop,
+          },
+          0
+        )
+        .to(
+          this.$refs.activeAvatarBack,
+          DURATION,
+          {
+            opacity: 0,
+          },
+          0
+        )
+        .to(
+          this.$refs.activeName,
+          DURATION,
+          {
+            paddingLeft: avatarStyle.width + PADDING,
+            y: this.messageTop,
+            fontSize: '1.25rem',
+            lineHeight: `${avatarStyle.height}px`,
+          },
+          0
+        )
+        .to(
+          this.$refs.activeDetails,
+          DURATION,
+          {
+            opacity: 0,
+            y: avatarStyle.height,
+          },
+          0
+        )
     },
     handleScroll: function(e) {
-      const contactIndex = Math.floor(this.$refs.contacts.scrollTop / 50)
-      this.updateActiveContact(contactIndex)
+      const { $refs, updateActiveContact } = this
+      const contactIndex = Math.floor($refs.contacts.scrollTop / CONTACTHEIGHT)
+      updateActiveContact(contactIndex)
       this.updating = false
     },
     update: function(e) {
-      if (this.updating) return
+      const { handleScroll, updating } = this
+      if (updating) return
       else {
-        requestAnimationFrame(this.handleScroll)
+        // use raf to improve scroll performance
+        requestAnimationFrame(handleScroll)
         this.updating = true
       }
-    }
+    },
   },
   mounted: function() {
-    this.$el.classList.remove('phonebook--loading')
+    this.$el.classList.remove(CLASSES.LOADING)
     this.$refs.contacts.addEventListener('scroll', this.update)
-  }
+  },
 })
