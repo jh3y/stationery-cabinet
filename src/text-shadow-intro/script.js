4894b91a3b1e9bf4c135e9a4e8ad7528c88b2141
jh3y <jh3y@users.noreply.github.com>
2018-06-11 15:23:17 +0100

    Updated text-shadow entrance

--- src/text-shadow-intro/script.js
+++ src/text-shadow-intro/script.js
@@ -1,3 +1,15 @@
-;(function(){
-  console.info('New pen created!');
-})();
+const spans = document.querySelectorAll('span')
+let ready = false
+const replay = () => {
+  if (ready) {
+    setTimeout(() => {
+      spans.forEach(s => s.classList.toggle('play'))
+      ready = false
+      setTimeout(() => spans.forEach(s => s.classList.toggle('play')), 0)
+    }, 2000)
+  }
+  if (!ready) {
+    ready = true
+  }
+}
+spans[spans.length - 1].addEventListener('animationend', replay)
