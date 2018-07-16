14b932b147c9000af318ff168e6fb14d3bef5198
jh3y <jh3y@users.noreply.github.com>
2018-07-16 03:51:51 +0100

    Update password gen to React

--- src/cpc-forms-password-gen/script.js
+++ src/cpc-forms-password-gen/script.js
@@ -1,9 +1,12 @@
+const { Component, Fragment } = window.React
+const { render } = window.ReactDOM
+const { default: styled } = window.styled
+const rootNode = document.getElementById('app')
+
 const symbolsSet = ' !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split('')
 const lowerSet = 'abcdefghijklmnopqrstuvwxyz'.split('')
 const upperSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 const numbersSet = '0123456789'.split('')
-const { Vue } = window
-
 const getRandomInRange = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min
 
@@ -36,68 +39,186 @@ const getPasswordStrength = password => {
   if (symbolTest.test(password)) strength += 1
   switch (strength) {
     case 1:
-      strength = '👎'
+      // strength = '👎'
+      strength = '#f9690e'
       break
     case 2:
-      strength = '😐'
+      // strength = '😐'
+      strength = '#f9bf3b'
       break
     case 3:
-      strength = '😄'
+      // strength = '😄'
+      strength = '#66cc99'
       break
     case 4:
-      strength = '💪'
+      // strength = '💪'
+      strength = '#00e640'
       break
+    default:
+      strength = '#f22613'
   }
   return strength
 }
-const defaultOptions = {
+const characters = getCharacters({
   lower: true,
   numbers: true,
-  symbols: true,
+  symbols: false,
   upper: false,
-}
-new Vue({
-  el: '#app',
-  data: {
-    characters: getCharacters(defaultOptions),
-    passLength: 32,
-    password: undefined,
+})
+const Dialog = styled.dialog`
+  background: ${p => getPasswordStrength(p.password)};
+  border: 6px solid ${p => getPasswordStrength(p.password)};
+`
+const PotentialPassword = styled.h1`
+  color: ${p => getPasswordStrength(p.password)};
+`
+const Action = styled.button`
+  background: ${p => (p.isIcon ? 'transparent' : 'dodgerblue')};
+  min-width: ${p => (p.isIcon ? '44px' : '120px')};
+  transition: background 0.25s ease;
+
+  &:hover {
+    background: ${p => (p.isIcon ? 'transparent' : '#1e8bc3')};
+  }
+
+  &:active {
+    background: ${p => (p.isIcon ? 'transparent' : '#3a539b')};
+  }
+`
+class App extends Component {
+  state = {
+    password: '',
     potential: {
       password: undefined,
       strength: undefined,
     },
-    options: defaultOptions,
-  },
-  methods: {
-    getNewPassword: function() {
-      const password = generatePassword(this.characters, this.passLength)
-      const strength = getPasswordStrength(password)
-      this.potential = {
+    options: {
+      characters,
+      lower: true,
+      numbers: true,
+      symbols: false,
+      upper: false,
+      length: 32,
+    },
+  }
+  getNewPassword = cb => {
+    const { characters, length } = this.state.options
+    const password = generatePassword(characters, length)
+    const strength = getPasswordStrength(password)
+    const potential = {
+      password,
+      strength,
+    }
+    this.setState(
+      {
+        potential,
+      },
+      () => {
+        if (typeof cb === 'function') cb()
+      }
+    )
+  }
+  setPassword = () => {
+    this.setState(
+      {
+        password: this.state.potential.password,
+      },
+      () => {
+        this.DIALOG.close()
+      }
+    )
+  }
+  onOptionChange = e => {
+    const { options: oldOptions } = this.state
+    const changedOption = {}
+    changedOption[e.target.name] =
+      e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    const options = Object.assign({}, oldOptions, changedOption)
+    if (e.target.type === 'checkbox')
+      options.characters = getCharacters(options)
+    const password = generatePassword(options.characters, options.length)
+    const strength = getPasswordStrength(password)
+    this.setState({
+      options,
+      potential: {
         password,
         strength,
-      }
-    },
-    openModal: function() {
-      this.getNewPassword()
-      this.$refs.MODAL.showModal()
-    },
-    setPassword: function() {
-      this.password = this.potential.password
-      this.$refs.MODAL.close()
-    },
-  },
-  watch: {
-    passLength: {
-      handler: function() {
-        this.getNewPassword()
       },
-    },
-    options: {
-      handler: function() {
-        this.characters = getCharacters(this.options)
-        this.getNewPassword()
-      },
-      deep: true,
-    },
-  },
-})
+    })
+  }
+  render = () => {
+    const { getNewPassword, onOptionChange, setPassword, state } = this
+    const { password, potential, options } = state
+    return (
+      <Fragment>
+        <Dialog
+          className="dialog"
+          innerRef={d => (this.DIALOG = d)}
+          password={potential.password}>
+          <div className="dialog__content" password={potential.password}>
+            <div className="potential">
+              <PotentialPassword password={potential.password}>
+                {potential.password}
+              </PotentialPassword>
+              {/*<PotentialStrength>{potential.strength}</PotentialStrength>*/}
+            </div>
+            <div className="options">
+              <label style={{ gridColumn: '1 / 4' }}>Length</label>
+              <label style={{ width: '30px' }}>{options.length}</label>
+              <input
+                type="range"
+                min="8"
+                max="100"
+                name="length"
+                onChange={onOptionChange}
+                style={{ gridColumn: '1 / -1' }}
+                value={options.length}
+              />
+              {[
+                { label: 'A-Z', id: 'upper' },
+                { label: 'a-z', id: 'lower' },
+                { label: '0-9', id: 'numbers' },
+                { label: '!"#$', id: 'symbols' },
+              ].map((o, i) => (
+                <Fragment key={`password-option--${i}`}>
+                  <label htmlFor={o.id}>{o.label}</label>
+                  <input
+                    id={o.id}
+                    type="checkbox"
+                    name={o.id}
+                    onChange={onOptionChange}
+                    checked={options[o.id]}
+                  />
+                </Fragment>
+              ))}
+            </div>
+            <div className="actions">
+              <Action isIcon={true} onClick={getNewPassword}>
+                <svg preserveAspectRatio="xMinYMin" viewBox="0 0 24 24">
+                  <path d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z" />
+                </svg>
+              </Action>
+              <Action onClick={setPassword}>Use</Action>
+            </div>
+          </div>
+        </Dialog>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={e =>
+              this.setState({
+                password: e.target.value,
+              })
+            }
+          />
+          <Action onClick={() => getNewPassword(() => this.DIALOG.showModal())}>
+            Generate
+          </Action>
+        </div>
+      </Fragment>
+    )
+  }
+}
+render(<App />, rootNode)
