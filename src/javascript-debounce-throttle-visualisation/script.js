0fa008be0a814d95d24943e626c8578372195289
jh3y <jh3y@users.noreply.github.com>
2018-08-24 00:40:35 +0100

    Updates for throttle/debounce visualisation w/ PIXI

--- src/javascript-debounce-throttle-visualisation/script.js
+++ src/javascript-debounce-throttle-visualisation/script.js
@@ -1,130 +1,213 @@
-const { PIXI, React, ReactDOM } = window
+const { PIXI, PropTypes, React, ReactDOM } = window
 const { Component, Fragment } = React
 const { render } = ReactDOM
-const { Application, Graphics } = PIXI
+const { Application, Graphics, Rectangle, Sprite } = PIXI
 const rootNode = document.getElementById('app')
 
 /**
  * debounce function
  * use inDebounce to maintain internal reference of timeout to clear
  */
-// const debounce = (func, delay) => {
-//   let inDebounce
-//   return function() {
-//     const context = this
-//     const args = arguments
-//     clearTimeout(inDebounce)
-//     inDebounce = setTimeout(() => func.apply(context, args), delay)
-//   }
-// }
+const debounce = (func, delay) => {
+  let inDebounce
+  return function() {
+    const context = this
+    const args = arguments
+    clearTimeout(inDebounce)
+    inDebounce = setTimeout(() => func.apply(context, args), delay)
+  }
+}
 
 /**
  * throttle function that catches and triggers last invocation
  * use time to see if there is a last invocation
  */
-// const throttle = (func, limit) => {
-//   let lastFunc
-//   let lastRan
-//   return function() {
-//     const context = this
-//     const args = arguments
-//     if (!lastRan) {
-//       func.apply(context, args)
-//       lastRan = Date.now()
-//     } else {
-//       clearTimeout(lastFunc)
-//       lastFunc = setTimeout(function() {
-//         if (Date.now() - lastRan >= limit) {
-//           func.apply(context, args)
-//           lastRan = Date.now()
-//         }
-//       }, limit - (Date.now() - lastRan))
-//     }
-//   }
-// }
+const throttle = (func, limit) => {
+  let lastFunc
+  let lastRan
+  return function() {
+    const context = this
+    const args = arguments
+    if (!lastRan) {
+      func.apply(context, args)
+      lastRan = Date.now()
+    } else {
+      clearTimeout(lastFunc)
+      lastFunc = setTimeout(function() {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args)
+          lastRan = Date.now()
+        }
+      }, limit - (Date.now() - lastRan))
+    }
+  }
+}
 
 class Timeline extends Component {
   state = {
     blobs: [],
   }
+  static defaultProps = {
+    blobSize: 20,
+    height: 100,
+  }
+  static propTypes = {
+    blobSize: PropTypes.number,
+    height: PropTypes.number,
+  }
   onTick = () => {
     const {
-      app,
-      blobContainer,
+      App,
+      BlobContainer,
       state: { blobs },
+      props: { blobSize, height },
     } = this
     if (!blobs || !blobs.length) return
-    if (app.renderer.width !== innerWidth) {
-      app.renderer.resize(innerWidth, 400)
-      blobContainer.removeChildren()
+    if (App.renderer.width !== innerWidth) {
+      App.renderer.resize(innerWidth, height)
+      BlobContainer.removeChildren()
     }
     for (const blob of blobs) {
-      if (!blob.active) {
-        blob.active = true
-        const blobG = new PIXI.Sprite(this.BASE)
-        blobG.x = 0
-        blobG.y = 0
-        blobG.tint = blobs.length > 3 ? 0xe74c3c : 0xffffff
-        let text = new PIXI.Text('This is a PixiJS text', {
-          fontFamily: 'Arial',
-          fontSize: 24,
-          fill: 0xff1010,
-          align: 'center',
-        })
-        blobG.text = 'Hello World'
-        blobG.addChild(text)
-        blobContainer.addChild(blobG)
-        // debugger
-      }
       // debugger
+      if (blob.active) {
+        blob.x += 1
+        blob.hitArea = new Rectangle(blob.x, blob.y, blobSize, blobSize)
+      }
+      if (blob.x > innerWidth) {
+        blob.active = false
+        blob.x = -blobSize
+        blob.data.fresh = false
+      }
     }
   }
-  addBlob = () => {
-    this.setState({
-      blobs: [...this.state.blobs, { t: new Date() }],
+  getBlob = isDebounce => {
+    const {
+      createBlob,
+      state: { blobs },
+    } = this
+    // Either generate a new blob or return one from state 👍
+    const available = blobs.find(b => !b.active)
+    if (available) {
+      available.active = true
+      if (isDebounce) available.tint = 0xe74c3c
+      return available
+    } else {
+      return createBlob()
+    }
+  }
+  createBlob = () => {
+    const {
+      BaseTexture,
+      BlobContainer,
+      props: { blobSize, height },
+    } = this
+    const Blob = new Sprite(BaseTexture)
+    const x = Math.floor(Math.random() * innerWidth)
+    const y = Math.floor(Math.random() * height)
+    Blob.active = true
+    Blob.x = -blobSize
+    Blob.y = y
+    Blob.tint = 0x2eec71
+    Blob.interactive = true
+    Blob.buttonMode = true
+    Blob.alpha = 0.5
+    Blob.hitArea = new Rectangle(x, y, blobSize, blobSize)
+    Blob.on('mouseover', () => {
+      Blob.alpha = 1
     })
+    Blob.on('mouseout', () => {
+      Blob.alpha = 0.5
+    })
+    Blob.data = {
+      fresh: true,
+      born: new Date().getTime(),
+    }
+    BlobContainer.addChild(Blob)
+    return Blob
+  }
+  showBlob = isDebounce => {
+    const {
+      getBlob,
+      state: { blobs: oldBlobs },
+    } = this
+    const newBlob = getBlob(isDebounce)
+    const blobs = newBlob.data.fresh
+      ? [...oldBlobs, newBlob]
+      : oldBlobs.map(b => (b.data.born === newBlob.data.born ? newBlob : b))
+    this.setState({ blobs })
+  }
+  createBlobContainer = () => {
+    const Container = new PIXI.particles.ParticleContainer(100, {
+      scale: true,
+      position: true,
+      alpha: true,
+    })
+    Container.buttonMode = true
+    Container.interactive = true
+    Container.interactiveChildren = true
+    return Container
+  }
+  createPixiApp = () => {
+    const {
+      VIEW: view,
+      props: { height },
+    } = this
+    const App = new Application({
+      antialias: true,
+      height,
+      transparent: true,
+      view,
+      width: innerWidth,
+    })
+    return App
+  }
+  createBaseTexture = PixiApp => {
+    const {
+      props: { blobSize },
+    } = this
+    const BlobGraphic = new Graphics()
+    BlobGraphic.beginFill(0xffffff)
+    BlobGraphic.drawCircle(0, 0, blobSize / 2)
+    BlobGraphic.endFill()
+    return PixiApp.renderer.generateTexture(BlobGraphic)
   }
   componentDidMount = () => {
-    const { onTick } = this
-    const app = (this.app = new Application({
-      antialias: true,
-      transparent: true,
-      view: this.VIEW,
-      width: innerWidth,
-      height: 400,
-    }))
-    const blobContainer = (this.blobContainer = new PIXI.particles.ParticleContainer(
-      100,
-      {
-        scale: true,
-        position: true,
-        alpha: true,
-      }
-    ))
-    app.stage.addChild(blobContainer)
-    app.ticker.add(onTick)
-    const p = new Graphics()
-    p.beginFill(0xffffff)
-    p.drawCircle(0, 0, 100)
-    p.endFill()
-    this.BASE = app.renderer.generateTexture(p)
+    const {
+      createBaseTexture,
+      createBlobContainer,
+      createPixiApp,
+      onTick,
+    } = this
+    const App = (this.App = createPixiApp())
+    const BlobContainer = (this.BlobContainer = createBlobContainer())
+    App.stage.addChild(BlobContainer)
+    App.ticker.add(onTick)
+    this.BaseTexture = createBaseTexture(App)
   }
   render = () => {
     return <canvas ref={view => (this.VIEW = view)} />
   }
 }
-
 class App extends Component {
-  addBlob = () => {
-    this.TIMELINE.addBlob()
+  state = {
+    debounce: 3000,
+    throttle: 2000,
   }
+  showDebounce = debounce(() => {
+    const { TIMELINE } = this
+    TIMELINE.showBlob(true)
+  }, this.state.debounce)
+  showThrottle = throttle(() => {
+    const { TIMELINE } = this
+    TIMELINE.showBlob()
+  }, this.state.throttle)
   render = () => {
-    const { addBlob } = this
+    const { showDebounce, showThrottle } = this
     return (
       <Fragment>
-        <h1>🌯</h1>
         <Timeline ref={timeline => (this.TIMELINE = timeline)} />
-        <button onClick={addBlob}>Add</button>
+        <button onClick={showDebounce}>Debounce</button>
+        <button onClick={showThrottle}>Throttle</button>
       </Fragment>
     )
   }
