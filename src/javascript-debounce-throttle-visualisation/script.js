8668ada7ec9b587a726e762a566ddfb3894f673b
jh3y <jh3y@users.noreply.github.com>
2018-08-26 03:31:24 +0100

    Finish debounce/throttle visualiser

--- src/javascript-debounce-throttle-visualisation/script.js
+++ src/javascript-debounce-throttle-visualisation/script.js
@@ -1,9 +1,13 @@
-const { PIXI, PropTypes, React, ReactDOM } = window
+const { moment, PIXI, PropTypes, React, ReactDOM } = window
 const { Component, Fragment } = React
 const { render } = ReactDOM
-const { Application, Graphics, Rectangle, Sprite } = PIXI
+const { Application, Rectangle, Sprite } = PIXI
 const rootNode = document.getElementById('app')
-
+const BLOB_SIZE = 40
+const DEBOUNCE = 'DEBOUNCE'
+const THROTTLE = 'THROTTLE'
+const TIME_FORMAT = 'hh:mm:ss'
+const TIME_BASE = moment().format(TIME_FORMAT)
 /**
  * debounce function
  * use inDebounce to maintain internal reference of timeout to clear
@@ -43,40 +47,73 @@ const throttle = (func, limit) => {
   }
 }
 
+const getShape = (size, isDiamond) => {
+  const d = document.createElement('canvas')
+  const c = d.getContext('2d')
+  const side = Math.sqrt((size * size) / 2)
+  d.height = size
+  d.width = size
+  c.fillStyle = '#ffffff'
+  if (isDiamond) {
+    c.translate(size / 2, 0)
+    c.rotate((45 * Math.PI) / 180)
+    c.fillRect(0, 0, side, side)
+  } else {
+    c.arc(size / 2, size / 2, size / 2, 0, 2 * Math.PI)
+    c.fill()
+  }
+  return d
+}
+const Circle = getShape(BLOB_SIZE)
+const Diamond = getShape(BLOB_SIZE, true)
 class Timeline extends Component {
   state = {
     blobs: [],
   }
   static defaultProps = {
-    blobSize: 20,
-    height: 100,
+    blobSize: BLOB_SIZE,
+    height: innerHeight,
+    debounceColor: 0x4ecdc4,
+    throttleColor: 0xf64747,
+    blobOpacity: 0.75,
+    onBlobSelect: () => {},
   }
   static propTypes = {
     blobSize: PropTypes.number,
     height: PropTypes.number,
+    debounceColor: PropTypes.number,
+    throttleColor: PropTypes.number,
+    blobOpacity: PropTypes.number,
+    onBlobSelect: PropTypes.func,
   }
   onTick = () => {
     const {
       App,
       BlobContainer,
+      BlobTwoContainer,
       state: { blobs },
-      props: { blobSize, height },
+      props: { blobSize },
     } = this
     if (!blobs || !blobs.length) return
-    if (App.renderer.width !== innerWidth) {
-      App.renderer.resize(innerWidth, height)
+    if (
+      App.renderer.width !== innerWidth ||
+      App.renderer.height !== innerHeight
+    ) {
+      App.renderer.resize(innerWidth, innerHeight)
       BlobContainer.removeChildren()
-    }
-    for (const blob of blobs) {
-      // debugger
-      if (blob.active) {
-        blob.x += 1
-        blob.hitArea = new Rectangle(blob.x, blob.y, blobSize, blobSize)
-      }
-      if (blob.x > innerWidth) {
-        blob.active = false
-        blob.x = -blobSize
-        blob.data.fresh = false
+      BlobTwoContainer.removeChildren()
+      this.setState({ blobs: [] })
+    } else {
+      for (const blob of blobs) {
+        if (blob.active) {
+          blob.x += 1
+          blob.hitArea = new Rectangle(blob.x, blob.y, blobSize, blobSize)
+        }
+        if (blob.x > innerWidth) {
+          blob.active = false
+          blob.x = -blobSize
+          blob.data.fresh = false
+        }
       }
     }
   }
@@ -86,43 +123,36 @@ class Timeline extends Component {
       state: { blobs },
     } = this
     // Either generate a new blob or return one from state 👍
-    const available = blobs.find(b => !b.active)
+    const available = blobs.find(
+      b => !b.active && (isDebounce ? b.type === DEBOUNCE : b.type === THROTTLE)
+    )
     if (available) {
       available.active = true
-      if (isDebounce) available.tint = 0xe74c3c
       return available
     } else {
-      return createBlob()
+      return createBlob(isDebounce)
     }
   }
-  createBlob = () => {
+  createBlob = isDebounce => {
     const {
-      BaseTexture,
       BlobContainer,
-      props: { blobSize, height },
+      BlobTwoContainer,
+      props: { blobSize, debounceColor, height, throttleColor },
     } = this
-    const Blob = new Sprite(BaseTexture)
-    const x = Math.floor(Math.random() * innerWidth)
-    const y = Math.floor(Math.random() * height)
+    const Blob = new Sprite.from(isDebounce ? Circle : Diamond)
+    const y = Math.floor(Math.random() * (height - blobSize))
     Blob.active = true
     Blob.x = -blobSize
     Blob.y = y
-    Blob.tint = 0x2eec71
-    Blob.interactive = true
-    Blob.buttonMode = true
-    Blob.alpha = 0.5
-    Blob.hitArea = new Rectangle(x, y, blobSize, blobSize)
-    Blob.on('mouseover', () => {
-      Blob.alpha = 1
-    })
-    Blob.on('mouseout', () => {
-      Blob.alpha = 0.5
-    })
+    Blob.tint = isDebounce ? debounceColor : throttleColor
+    Blob.alpha = 1
     Blob.data = {
       fresh: true,
       born: new Date().getTime(),
+      type: isDebounce ? DEBOUNCE : THROTTLE,
     }
-    BlobContainer.addChild(Blob)
+    if (isDebounce) BlobContainer.addChild(Blob)
+    else BlobTwoContainer.addChild(Blob)
     return Blob
   }
   showBlob = isDebounce => {
@@ -137,14 +167,11 @@ class Timeline extends Component {
     this.setState({ blobs })
   }
   createBlobContainer = () => {
-    const Container = new PIXI.particles.ParticleContainer(100, {
+    const Container = new PIXI.particles.ParticleContainer(10000, {
       scale: true,
       position: true,
       alpha: true,
     })
-    Container.buttonMode = true
-    Container.interactive = true
-    Container.interactiveChildren = true
     return Container
   }
   createPixiApp = () => {
@@ -161,28 +188,14 @@ class Timeline extends Component {
     })
     return App
   }
-  createBaseTexture = PixiApp => {
-    const {
-      props: { blobSize },
-    } = this
-    const BlobGraphic = new Graphics()
-    BlobGraphic.beginFill(0xffffff)
-    BlobGraphic.drawCircle(0, 0, blobSize / 2)
-    BlobGraphic.endFill()
-    return PixiApp.renderer.generateTexture(BlobGraphic)
-  }
   componentDidMount = () => {
-    const {
-      createBaseTexture,
-      createBlobContainer,
-      createPixiApp,
-      onTick,
-    } = this
+    const { createBlobContainer, createPixiApp, onTick } = this
     const App = (this.App = createPixiApp())
     const BlobContainer = (this.BlobContainer = createBlobContainer())
+    const BlobTwoContainer = (this.BlobTwoContainer = createBlobContainer())
     App.stage.addChild(BlobContainer)
+    App.stage.addChild(BlobTwoContainer)
     App.ticker.add(onTick)
-    this.BaseTexture = createBaseTexture(App)
   }
   render = () => {
     return <canvas ref={view => (this.VIEW = view)} />
@@ -190,24 +203,90 @@ class Timeline extends Component {
 }
 class App extends Component {
   state = {
-    debounce: 3000,
-    throttle: 2000,
+    time: TIME_BASE,
+    debounceRate: 2000,
+    throttleRate: 1000,
   }
-  showDebounce = debounce(() => {
-    const { TIMELINE } = this
-    TIMELINE.showBlob(true)
-  }, this.state.debounce)
-  showThrottle = throttle(() => {
-    const { TIMELINE } = this
-    TIMELINE.showBlob()
-  }, this.state.throttle)
+  showBlob = isDebounce => {
+    return () => {
+      this.TIMELINE.showBlob(isDebounce)
+    }
+  }
+  tick = () => {
+    this.setState({
+      time: moment().format(TIME_FORMAT),
+    })
+  }
+  showThrottle = throttle(this.showBlob(), this.state.throttleRate)
+  showDebounce = debounce(this.showBlob(true), this.state.debounceRate)
+  componentDidMount = () => {
+    window.addEventListener('mousemove', this.showDebounce)
+    window.addEventListener('mousemove', this.showThrottle)
+    window.addEventListener('touchmove', this.showDebounce)
+    window.addEventListener('touchmove', this.showThrottle)
+    this.INTERVAL = setInterval(this.tick, 1000)
+  }
+  componentWillUnmount = () => {
+    window.removeEventListener('mousemove', this.showDebounce)
+    window.removeEventListener('mousemove', this.showThrottle)
+    window.removeEventListener('touchmove', this.showDebounce)
+    window.removeEventListener('touchmove', this.showThrottle)
+    clearInterval(this.INTERVAL)
+  }
+  updateHandlers = debounce(() => {
+    const {
+      state: { debounceRate, throttleRate },
+    } = this
+    window.removeEventListener('mousemove', this.showDebounce)
+    window.removeEventListener('mousemove', this.showThrottle)
+    window.removeEventListener('touchmove', this.showDebounce)
+    window.removeEventListener('touchmove', this.showThrottle)
+    this.showThrottle = throttle(this.showBlob(), throttleRate)
+    this.showDebounce = debounce(this.showBlob(true), debounceRate)
+    window.addEventListener('mousemove', this.showDebounce)
+    window.addEventListener('mousemove', this.showThrottle)
+    window.addEventListener('touchmove', this.showDebounce)
+    window.addEventListener('touchmove', this.showThrottle)
+  }, 500)
+  onOptionChange = e => {
+    e.persist()
+    const newState = {}
+    newState[e.target.name] = e.target.value
+    this.setState(newState, () => this.updateHandlers(e.target.value))
+  }
   render = () => {
-    const { showDebounce, showThrottle } = this
+    const {
+      onOptionChange,
+      state: { debounceRate, throttleRate, time },
+    } = this
     return (
       <Fragment>
-        <Timeline ref={timeline => (this.TIMELINE = timeline)} />
-        <button onClick={showDebounce}>Debounce</button>
-        <button onClick={showThrottle}>Throttle</button>
+        <div className="timeline__container">
+          <div className="time">{time}</div>
+          <Timeline ref={timeline => (this.TIMELINE = timeline)} />
+        </div>
+        <div className="actions">
+          <label>{`Debounce for ${debounceRate}ms`}</label>
+          <input
+            type="range"
+            min="50"
+            max="10000"
+            step="50"
+            name="debounceRate"
+            onChange={onOptionChange}
+            value={debounceRate}
+          />
+          <label>{`Throttle every ${throttleRate}ms`}</label>
+          <input
+            type="range"
+            min="50"
+            max="10000"
+            step="50"
+            name="throttleRate"
+            onChange={onOptionChange}
+            value={throttleRate}
+          />
+        </div>
       </Fragment>
     )
   }
