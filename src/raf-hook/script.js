26b99cf87d54fc4672380f865b961afb297c550a
jh3y <jh3y@users.noreply.github.com>
2019-02-12 16:53:07 +0000

    Updates to raf hook to share frame

--- src/raf-hook/script.js
+++ src/raf-hook/script.js
@@ -1,16 +1,36 @@
 const { React, ReactDOM } = window
-const { useEffect, useState } = React
+const { useEffect, useRef, useState } = React
 const { render } = ReactDOM
 const rootNode = document.getElementById('app')
-
+/**
+ * In house debounce
+ * @param {function} func - callback to be fired
+ * @param {number} delay - delay in ms
+ */
+const debounce = (func, delay) => {
+  let inDebounce
+  return function() {
+    const context = this
+    const args = arguments
+    clearTimeout(inDebounce)
+    inDebounce = setTimeout(() => func.apply(context, args), delay)
+  }
+}
 /**
  * A hook for wrapping callbacks inside requestAnimationFrame for performance
  * @param {function} callback - a callback to use inside the raf handler
  */
-const useRAF = callback => {
+const useRAF = (callback, reset = 500) => {
   const [handler, setHandler] = useState()
+  const rafId = useRef(0)
+  const event = useRef(null)
+  let tick = useRef(0)
+  const resetInternal = () => {
+    tick.current = 0
+    callback(event.current, 0)
+  }
   useEffect(() => {
-    let rafId
+    const onFinish = debounce(resetInternal, reset)
     setHandler(() => {
       let active = false
       return e => {
@@ -19,15 +39,19 @@ const useRAF = callback => {
         } else {
           active = true
           const onFrame = () => {
-            callback(e)
+            callback(e, tick.current)
             active = false
+            onFinish()
           }
-          rafId = requestAnimationFrame(onFrame)
+          event.current = e
+          tick.current++
+          rafId.current = requestAnimationFrame(onFrame)
         }
       }
     })
     return () => {
-      cancelAnimationFrame(rafId)
+      cancelAnimationFrame(rafId.current)
+      rafId.current = undefined
     }
   }, [])
   return handler
@@ -36,9 +60,11 @@ const useRAF = callback => {
 const App = () => {
   const [x, setX] = useState(0)
   const [y, setY] = useState(0)
-  const handler = useRAF(e => {
+  const [frame, setFrame] = useState(0)
+  const handler = useRAF((e, frame) => {
     setX(e.x)
     setY(e.y)
+    setFrame(frame)
   })
   useEffect(() => {
     window.addEventListener('mousemove', handler)
@@ -46,7 +72,7 @@ const App = () => {
       window.removeEventListener('mousemove', handler)
     }
   })
-  return <h1>{`x: ${x}; y: ${y}`}</h1>
+  return <h1>{`x: ${x}; y: ${y}; frame: ${frame}`}</h1>
 }
 
 render(<App />, rootNode)
