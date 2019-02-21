48aa968bda90f424fefa5f4a93653829f10ab914
jh3y <jh3y@users.noreply.github.com>
2019-02-21 00:40:18 +0000

    Updates + infinite triangles

--- src/dont-feel-good/script.js
+++ src/dont-feel-good/script.js
@@ -3,13 +3,13 @@ const bearCells = bearGrid.querySelectorAll('.bear-grid__cell')
 const lastCell = bearCells[bearCells.length - 1]
 const replay = document.querySelector('.replay')
 
-
-const imageHasLoaded = (src) => new Promise((resolve, reject) => {
-  const img = new Image()
-  img.onload = resolve
-  img.onerror = reject
-  img.src = src
-})
+const imageHasLoaded = src =>
+  new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = resolve
+    img.onerror = reject
+    img.src = src
+  })
 
 const showReset = () => {
   bearGrid.classList.toggle('bear-grid--fading')
@@ -21,11 +21,9 @@ const fade = () => {
   bearGrid.classList.toggle('bear-grid--fading')
 }
 
-
 lastCell.addEventListener('animationend', showReset)
 replay.addEventListener('click', fade)
 
-imageHasLoaded('https://jh3y-doodles.netlify.com/dontfeelgoodbear.svg')
-  .then(() => {
-    bearGrid.classList.toggle('bear-grid--fading')
-  })
+imageHasLoaded('./dontfeelgoodbear.svg').then(() => {
+  bearGrid.classList.toggle('bear-grid--fading')
+})
