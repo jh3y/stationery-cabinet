1af6247993617cea16ffda26df667f8faae36803
jh3y <jh3y@users.noreply.github.com>
2019-07-27 04:10:35 +0100

    feat: sparkler updates to try out updating color on hover

--- src/pure-css-sparkler/script.js
+++ src/pure-css-sparkler/script.js
@@ -1,10 +1,24 @@
 /**
  * JavaScript only used to update sparkler position 😅
  */ const root = document.documentElement
-const update = ({ x, y }) => {
+const update = e => {
+  let x
+  let y
+  /* eslint-disable */ if (window.PointerEvent) {
+    ;({ x, y } = e)
+  } else {
+    ;({ pageX: x, pageY: y } = e.changedTouches[0])
+  }
   root.style.setProperty('--x', x)
   root.style.setProperty('--y', y)
 }
-update({ x: innerWidth / 2, y: innerHeight / 2 })
-window.addEventListener('pointermove', update)
-window.addEventListener('pointerdown', update)
+update({
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+  changedTouches: [{ pageX: innerWidth / 2, pageY: innerHeight / 2 }],
+})
+const move = window.PointerEvent ? 'pointermove' : 'touchmove'
+const down = window.PointerEvent ? 'pointerdown' : 'touchstart'
+if (!window.PointerEvent) document.body.style.touchAction = 'auto'
+window.addEventListener(move, update)
+window.addEventListener(down, update)
