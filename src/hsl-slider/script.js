bdf8e598f4f7037fc829868ee975e4275ae3cb79
jh3y <jh3y@users.noreply.github.com>
2019-09-10 23:04:22 +0100

    feat: add lightness slider foundation

--- src/hsl-slider/script.js
+++ src/hsl-slider/script.js
@@ -5,8 +5,10 @@ const rootNode = document.getElementById('app')
 
 const ColorSlider = () => {
   const [value, setValue] = useState(0)
+  const [lightness, setLightness] = useState(50)
   const handleRef = useRef(null)
   const trackRef = useRef(null)
+  const lightnessHandleRef = useRef(null)
 
   const updateValue = ({ x }) => {
     const {
@@ -20,32 +22,53 @@ const ColorSlider = () => {
   }
 
   // Set up event bindings on the handle
-  const handleDown = () => {
-    window.addEventListener('pointermove', updateValue)
-    // Need a ref to the parent div bar, then calculate the distance covered at the pointer in relation to the whole thing 👍
-  }
   const handleUp = () => {
     window.removeEventListener('pointermove', updateValue)
+    window.removeEventListener('pointerup', handleUp)
+  }
+  const handleDown = () => {
+    window.addEventListener('pointermove', updateValue)
+    window.addEventListener('pointerup', handleUp)
+    // Need a ref to the parent div bar, then calculate the distance covered at the pointer in relation to the whole thing 👍
   }
   useEffect(() => {
     // console.info(handleRef.current)
     handleRef.current.addEventListener('pointerdown', handleDown)
-    handleRef.current.addEventListener('pointerup', handleUp)
     return () => {
       handleRef.current.removeEventListener('pointerdown', handleDown)
-      handleRef.current.removeEventListener('pointerup', handleUp)
     }
   }, [])
+  useEffect(() => {
+    lightnessHandleRef.current.addEventListener('pointerdown', e =>
+      e.stopPropagation()
+    )
+    lightnessHandleRef.current.addEventListener('input', e => {
+      e.stopPropagation()
+      setLightness(e.target.value)
+    })
+  }, [])
   return (
-    <div ref={trackRef} className="color-slider">
+    <div
+      ref={trackRef}
+      className="color-slider"
+      style={{
+        '--lightness': lightness,
+      }}>
       <div
         className="color-slider__handle"
         ref={handleRef}
         style={{
           '--value': Math.max(0, Math.min(100, (value / 360) * 100)),
           '--hue': value,
-        }}
-      />
+        }}>
+        <input
+          ref={lightnessHandleRef}
+          type="range"
+          step="1"
+          min="0"
+          max="100"
+        />
+      </div>
     </div>
   )
 }
