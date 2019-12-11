ab68b5310194c3ac01e4eb0c32ea7c0a57f25cbb
jh3y <jh3y@users.noreply.github.com>
2019-12-11 20:31:29 +0000

    feat: glitchy text reveal update

--- src/glitchy-text-reveal/script.js
+++ src/glitchy-text-reveal/script.js
@@ -1,14 +1,18 @@
 const { Splitting } = window
-const results = Splitting()
-const glitches = '`¡™£¢∞§¶•ªº–≠åß∂ƒ©˙∆˚¬…æ≈ç√∫˜µ≤≥÷/?░▒▓<>/'.split('')
-for (let r = 0; r < results.length; r++) {
-  const chars = results[r].chars
-  for (let c = 0; c < chars.length; c++) {
-    chars[c].style.setProperty('--count', Math.random() * 5 + 1)
+const RESULTS = Splitting()
+// Set of characters we can use to glitch through
+const GLITCH_CHARS = '`¡™£¢∞§¶•ªº–≠åß∂ƒ©˙∆˚¬…æ≈ç√∫˜µ≤≥÷/?░▒▓<>/'.split('')
+// Loop through our Splitting results and apply CSS variables.
+// The results contain an Array of spans that are the characters.
+for (let r = 0; r < RESULTS.length; r++) {
+  const CHARS = RESULTS[r].chars
+  for (let c = 0; c < CHARS.length; c++) {
+    CHARS[c].style.setProperty('--count', Math.random() * 5 + 1)
+    // We are going to inline 10 CSS variables
     for (let g = 0; g < 10; g++) {
-      chars[c].style.setProperty(
+      CHARS[c].style.setProperty(
         `--char-${g}`,
-        `"${glitches[Math.floor(Math.random() * glitches.length)]}"`
+        `"${GLITCH_CHARS[Math.floor(Math.random() * GLITCH_CHARS.length)]}"`
       )
     }
   }
