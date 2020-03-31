a4b30000b7bc044d3fd8f2cdb5af93a946fec271
jh3y <jh3y@users.noreply.github.com>
2020-03-31 23:00:25 +0100

    feat: rainbow cloud progress check in

--- src/rainbow-cloud/script.js
+++ src/rainbow-cloud/script.js
@@ -1,1 +1,60 @@
-// 404
+const {
+  gsap: { to, timeline, set },
+} = window
+
+const CONFIG = {
+  SPEED: {
+    CLOUD: 2,
+  },
+  COLORS: {
+    CLOUD: {
+      ON: 100,
+      OFF: 65,
+    },
+  },
+}
+
+const CLOUD_WRAPPER = document.querySelector('.cloud__wrapper')
+const CLOUD = document.querySelector('.cloud')
+const RAINBOW = document.querySelector('.rainbow')
+
+set(CLOUD_WRAPPER, { transformOrigin: '135% 55%' })
+set(CLOUD, {
+  transformOrigin: '50% 50%',
+  '--cloud-lightness': CONFIG.COLORS.CLOUD.OFF,
+})
+
+const CLOUD_TIMELINE = new timeline()
+  // Traveling over the arc
+  .add(
+    to(CLOUD_WRAPPER, {
+      duration: CONFIG.SPEED.CLOUD,
+      rotate: 180,
+      ease: 'Power4.easeIn',
+    })
+  )
+  // Counter rotating the cloud
+  .add(
+    to(CLOUD, {
+      duration: CONFIG.SPEED.CLOUD,
+      rotate: -180,
+      '--cloud-lightness': CONFIG.COLORS.CLOUD.ON,
+      ease: 'Power4.easeIn',
+    }),
+    0
+  )
+
+set(RAINBOW, { transformOrigin: '50% 100%', rotate: 180 })
+const RAINBOW_TL = to(RAINBOW, {
+  duration: 2,
+  rotate: 360,
+  ease: 'Power4.easeIn',
+})
+
+// This is the daddy
+const MASTER_TL = new timeline()
+
+// Add the cloud timeline
+MASTER_TL.add(CLOUD_TIMELINE).add(RAINBOW_TL, 0)
+
+// GSDevTools.create()
