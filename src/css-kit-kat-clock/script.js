353f67d12f3cce571e722b15d61d68d7625f2281
jh3y <jh3y@users.noreply.github.com>
2020-04-16 23:45:34 +0100

    feat: kit kat clock

--- src/css-kit-kat-clock/script.js
+++ src/css-kit-kat-clock/script.js
@@ -1,1 +1,11 @@
-// 404
+const CLOCK = document.querySelector('.clock')
+const DATE = new Date()
+const MINUTES = DATE.getMinutes()
+const HOURS = DATE.getHours() % 12
+
+CLOCK.style.setProperty('--hue', Math.random() * 360)
+CLOCK.style.setProperty('--minutes-delay', (3600 / 60) * MINUTES)
+CLOCK.style.setProperty(
+  '--hours-delay',
+  (43200 / 12) * HOURS + (43200 / 12 / 60) * MINUTES
+)
