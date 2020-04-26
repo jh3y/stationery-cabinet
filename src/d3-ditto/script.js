c92c4230e908d4b63d374ad921b30fb0f291a16a
jh3y <jh3y@users.noreply.github.com>
2020-04-26 22:09:58 +0100

    feat: ditto w/ d3

--- src/d3-ditto/script.js
+++ src/d3-ditto/script.js
@@ -1,21 +1,28 @@
 const {
-  d3: { curveBasisClosed, curveStep, curveCardinalClosed, lineRadial },
+  gsap: { timeline, set },
+  d3: { curveBasisClosed, curveStep, lineRadial },
 } = window
 
-const CURVES = [
-  curveBasisClosed,
-  curveStep,
-  // curveNatural,
-  // curveLinearClosed,
-  curveCardinalClosed,
-  // curveCatmullRomClosed,
+// Elements
+const DITTO = document.querySelector('.ditto')
+const DITTO_PATH = document.querySelector('.ditto__path')
+const DITTO_MOUTH = document.querySelector('.ditto__mouth')
+const DITTO_BEAM = document.querySelector('.ditto__outline')
+const POKEBALL_BEAM = document.querySelector('.pokeball__beam')
+const DITTO_REAL = document.querySelector('.ditto__real')
+const BUTTON = document.querySelector('button')
+// Configurations
+const CURVES = [curveBasisClosed, curveStep]
+const DITTO_MOUTHS = [
+  'M -40 -42.5 q 40 15 80 0', // Smile
+  'M -40 -35 q -20 -0 80 0', // Straight
 ]
-
 // Gonna say that Ditto has around 14 points
 // Linear Radial goes 0 -> 360 CW from 12
 const DEFAULT_POINTS = [
   [0, 100],
-  [30, 150],
+  [15, 120],
+  [30, 130],
   [50, 95],
   [70, 140],
   [80, 150],
@@ -29,14 +36,41 @@ const DEFAULT_POINTS = [
   [215, 130],
   [220, 160],
   [230, 180],
-  [250, 90],
-  [280, 180],
+  [270, 80],
+  [280, 160],
+  [300, 190],
   [315, 80],
-  [335, 180],
+  [325, 140],
+  [345, 160],
 ]
-const DITTO = document.querySelector('.ditto')
-const DITTO_PATH = document.querySelector('.ditto__path')
-const BUTTON = document.querySelector('button')
+
+const SQUARE_POINTS = [
+  [45, 150],
+  [135, 150],
+  [225, 150],
+  [315, 150],
+]
+
+const SWAY = {
+  X: 10,
+  Y: 40,
+}
+
+// Utility function
+const randomInRange = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min
+// Return new points for Ditto body
+// Maps the default points given some bounds
+const getPoints = () => {
+  if (Math.random() > 0.75) return SQUARE_POINTS
+  const POINTS = DEFAULT_POINTS.map(point => [
+    randomInRange(point[0] - SWAY.X, point[0] + SWAY.X),
+    randomInRange(point[1] - SWAY.Y, point[1] + SWAY.Y),
+  ])
+  return POINTS
+}
+// Draws an outline for Ditto and determines whether
+// he's smiling or what he color he is
 const drawDitto = (
   points = DEFAULT_POINTS,
   curveBasis = curveBasisClosed,
@@ -52,12 +86,109 @@ const drawDitto = (
   )
 }
 
+// Draw ditto initially, doesn't necessarily mean he's showing
 drawDitto()
 
+const STATE = {
+  ACTIVE: false,
+  DITTO_OUT: false,
+  RAN: false,
+}
+const CONFIG = {
+  POKEBALL_SPEED: 0.15,
+}
+set(DITTO_BEAM, { transformOrigin: '50% 50%', scale: 1.05, opacity: 0 })
+set(POKEBALL_BEAM, { transformOrigin: '50% 100%', scaleY: 0 })
+// Event binding
+const onComplete = () => {
+  BUTTON.removeAttribute('style')
+  STATE.DITTO_OUT = !STATE.DITTO_OUT
+  STATE.ACTIVE = false
+}
+const onStart = () => {
+  STATE.ACTIVE = true
+}
 BUTTON.addEventListener('click', () => {
-  drawDitto(
-    undefined,
-    CURVES[Math.floor(Math.random() * CURVES.length)],
-    Math.random() > 0.75 ? 180 : 320
-  )
+  if (STATE.ACTIVE) return
+  if (STATE.RAN && !STATE.DITTO_OUT) {
+    drawDitto(
+      Math.random() > 0.5 ? DEFAULT_POINTS : getPoints(),
+      Math.random() > 0.75 ? CURVES[1] : CURVES[0],
+      Math.random() > 0.75 ? 180 : 320
+    )
+  }
+  if (!STATE.RAN) STATE.RAN = true
+  set(BUTTON, { '--level': 0, transformOrigin: '50% 100%', rotateX: -20 })
+  if (STATE.DITTO_OUT) {
+    new timeline({
+      onStart,
+      onComplete,
+    })
+      .set(POKEBALL_BEAM, { transformOrigin: '50% 100%', opacity: 1 })
+      .to(DITTO_BEAM, { duration: CONFIG.POKEBALL_SPEED, opacity: 1 }, 0)
+      .to(POKEBALL_BEAM, { duration: CONFIG.POKEBALL_SPEED, scaleY: 1 }, 0)
+      .to(DITTO_REAL, { duration: CONFIG.POKEBALL_SPEED, opacity: 0 }, 0)
+      .to(
+        DITTO_BEAM,
+        { duration: CONFIG.POKEBALL_SPEED, opacity: 0 },
+        CONFIG.POKEBALL_SPEED
+      )
+      .to(
+        POKEBALL_BEAM,
+        { duration: CONFIG.POKEBALL_SPEED, scaleY: 0 },
+        CONFIG.POKEBALL_SPEED
+      )
+  } else {
+    new timeline({
+      onStart,
+      onComplete,
+    })
+      .to(POKEBALL_BEAM, { duration: CONFIG.POKEBALL_SPEED, scaleY: 1 }, 0)
+      .to(DITTO_BEAM, { duration: CONFIG.POKEBALL_SPEED, opacity: 1 }, 0)
+      .to(
+        DITTO_BEAM,
+        { duration: CONFIG.POKEBALL_SPEED, opacity: 0 },
+        CONFIG.POKEBALL_SPEED
+      )
+      .to(
+        DITTO_REAL,
+        { duration: CONFIG.POKEBALL_SPEED, opacity: 1 },
+        CONFIG.POKEBALL_SPEED
+      )
+      .to(
+        POKEBALL_BEAM,
+        {
+          duration: CONFIG.POKEBALL_SPEED,
+          opacity: 0,
+          transformOrigin: '50% 0',
+          scaleY: 0,
+        },
+        CONFIG.POKEBALL_SPEED
+      )
+  }
 })
+
+// Update his mouth
+const smileOrNoSmile = () => {
+  const MOUTH_INDEX = randomInRange(0, DITTO_MOUTHS.length - 1)
+  DITTO_MOUTH.setAttribute('d', DITTO_MOUTHS[MOUTH_INDEX])
+  setTimeout(smileOrNoSmile, randomInRange(4000, 10000))
+}
+smileOrNoSmile()
+
+const EYES = document.querySelector('.ditto__eyes')
+const blink = () => {
+  set(EYES, { scaleY: 1 })
+  if (EYES.BLINK_TL) EYES.BLINK_TL.kill()
+  EYES.BLINK_TL = new timeline({
+    delay: Math.floor(Math.random() * 4) + 1,
+    onComplete: () => blink(EYES),
+  }).to(EYES, {
+    duration: 0.05,
+    transformOrigin: '50% 50%',
+    scaleY: 0,
+    yoyo: true,
+    repeat: 1,
+  })
+}
+blink()
