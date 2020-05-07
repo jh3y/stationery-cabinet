0c40dc899fedb05cc29c788a8f5c1a6599ab057b
jh3y <jh3y@users.noreply.github.com>
2020-05-07 21:30:06 +0100

    feat: for Elon

--- src/sr-71/script.js
+++ src/sr-71/script.js
@@ -1,22 +1,143 @@
 const {
-  gsap: { timeline, set },
+  gsap: { set, timeline, to },
 } = window
 
-// 100 x to 58 y is the ratio for movement
+// 100 x to 57.75 y is the ratio for movement
+const shift = distance => ({ x: distance, y: distance * -0.5775 })
+const movementParams = shift(-622)
+const jetParams = shift(50)
+
+set('.dupe', { ...shift(-618) })
+set('.blackbird__shadow', { transformOrigin: '50% 50%', scale: 0.75 })
+set('.antennae__flash--higher', { transformOrigin: '50% 50%', x: 468, y: 705 })
+set('.antennae__flash--lower', { transformOrigin: '50% 50%', x: 480, y: 720 })
+
+const DRIFT_SPEED = 2
+const LOWER_SPEED = 5
+const SHIFT_SPEED = 2
+const RAISE_SPEED = 4
+
+document.querySelector('audio').volume = 0.5
+
 new timeline({
-  id: 'Main',
+  id: 'Landscape',
   repeat: -1,
-  onStart: () => {
-    set('.building', { opacity: 1 })
-    set('.landscape', { x: '-=500', y: '+=290' })
-  },
+}).to('.landscape', {
+  x: `-=${movementParams.x}`,
+  y: `-=${movementParams.y}`,
+  duration: 0.35,
+  ease: 'none',
 })
-  .to('.landscape', { ease: 'none', x: '+=1000', y: '-=580', duration: 1 })
+
+new timeline({
+  id: 'Blackbird',
+  delay: Math.random() + 1,
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: Math.random() * 2 + 1,
+})
   .to(
-    '.building',
-    { id: 'building', ease: 'none', opacity: 0, duration: 0.1 },
-    0.5
+    ['.blackbird', '.blackbird__shadow'],
+    {
+      x: `+=${jetParams.x}`,
+      y: `-=${jetParams.y}`,
+      duration: DRIFT_SPEED,
+      ease: 'Power2.easeInOut',
+    },
+    0
   )
-  .to('.mountain', { ease: 'none', opacity: 0, duration: 0.1 }, 0.85)
+  .to(
+    ['.blackbird'],
+    {
+      x: `+=${100}`,
+      y: `+=${200}`,
+      duration: LOWER_SPEED,
+      ease: 'Power2.easeInOut',
+    },
+    DRIFT_SPEED
+  )
+  .to(
+    ['.blackbird__shadow'],
+    {
+      x: '+=100',
+      scale: 0.9,
+      opacity: 0.85,
+      duration: LOWER_SPEED,
+      ease: 'Power2.easeInOut',
+    },
+    DRIFT_SPEED
+  )
+  .to(
+    ['.blackbird', '.blackbird__shadow'],
+    {
+      x: `-=${100}`,
+      y: `-=${50}`,
+      duration: SHIFT_SPEED,
+      ease: 'Power2.easeInOut',
+    },
+    LOWER_SPEED + DRIFT_SPEED
+  )
+  // .to('.')
+
+  .to(
+    ['.blackbird__shadow'],
+    {
+      x: `-=${jetParams.x}`,
+      y: `+=${jetParams.y + 50}`,
+      scale: 0.75,
+      opacity: 0.5,
+      duration: RAISE_SPEED,
+      ease: 'Power2.easeInOut',
+    },
+    LOWER_SPEED + DRIFT_SPEED + SHIFT_SPEED
+  )
+  .to(
+    ['.blackbird'],
+    {
+      x: `-=${jetParams.x}`,
+      y: `+=${jetParams.y - 150}`,
+      duration: RAISE_SPEED,
+      ease: 'Power2.easeInOut',
+    },
+    LOWER_SPEED + DRIFT_SPEED + SHIFT_SPEED
+  )
+
+to('.blackbird__body', {
+  duration: 0.2,
+  y: '+=2',
+  repeat: -1,
+  yoyo: true,
+  ease: 'none',
+})
+
+// Antennae flash
+new timeline({ repeat: -1 }).to('.antennae__flash', {
+  scale: 1,
+  stagger: 0.25,
+  duration: 0.5,
+})
+// Color switching
+const SWITCH_DELAY = 5
+new timeline({
+  delay: SWITCH_DELAY,
+  repeat: -1,
+  repeatDelay: SWITCH_DELAY,
+  yoyo: true,
+  ease: 'none',
+})
+  .to(document.documentElement, {
+    duration: 1,
+    '--blackbird-offset': 30,
+    '--sky-lightness': 10,
+    '--sky-saturation': 5,
+    '--mountain-lightness': 20,
+    '--ground-lightness': 15,
+    '--tree-leaves-lightness': 15,
+    '--tree-stump-lightness': 10,
+    '--road-lightness': 10,
+    '--road-alpha': 0.5,
+    '--building-lightness': 40,
+  })
+  .to(document.documentElement, { duration: 0, '--glass-hue': 60 }, 0.5)
 
 // GSDevTools.create()
