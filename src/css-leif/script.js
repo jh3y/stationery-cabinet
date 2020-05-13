124db4b0e45609cc848a33eed3190838d77f7d98
jh3y <jh3y@users.noreply.github.com>
2020-05-13 23:53:24 +0100

    feat: CSS Leif complete

--- src/css-leif/script.js
+++ src/css-leif/script.js
@@ -1,1 +1,6 @@
-// 404
+const restart = () => {
+  const MARKUP = document.body.innerHTML
+  document.body.innerHTML = ''
+  requestAnimationFrame(() => (document.body.innerHTML = MARKUP))
+}
+document.body.addEventListener('click', restart)
