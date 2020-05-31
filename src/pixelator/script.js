cf8db8427d5dc875d42b575d876cce164d9b3532
jh3y <jh3y@users.noreply.github.com>
2020-05-31 21:17:42 +0100

    feat: port all controls to dat.gui drawer

--- src/pixelator/script.js
+++ src/pixelator/script.js
@@ -1,6 +1,6 @@
 const {
-  React: { useState, useRef, useEffect },
-  ReactDOM: { render },
+  React: { Fragment, useCallback, useState, useRef, useEffect },
+  ReactDOM: { render, createPortal },
   styled,
   PropTypes: T,
   dat: { GUI },
@@ -12,6 +12,8 @@ const CONFIG = {
   width: 10,
   size: 10,
   color: '#e74c3c',
+  dark: false,
+  debug: false,
 }
 
 const Container = styled.div`
@@ -69,6 +71,14 @@ const PixelCanvas = ({ color, erase, cells, size, height, width }) => {
     }
   }, [height, width])
 
+  useEffect(() => {
+    for (let c = 0; c < cells.length; c++) {
+      gridRef.current.children[c].removeAttribute('style')
+      if (cells[c].color)
+        gridRef.current.children[c].style.setProperty('--color', cells[c].color)
+    }
+  }, [cells])
+
   return (
     <Grid
       onPointerDown={start}
@@ -80,9 +90,9 @@ const PixelCanvas = ({ color, erase, cells, size, height, width }) => {
       width={width}
       height={height}
       size={size}>
-      {cells.map((c, index) => (
-        <Cell key={index} data-index={index} index={index} />
-      ))}
+      {cells.map((c, index) => {
+        return <Cell key={index} data-index={index} index={index} />
+      })}
     </Grid>
   )
 }
@@ -99,6 +109,9 @@ PixelCanvas.propTypes = {
   height: T.number,
 }
 
+const OutputDrawer = styled.details``
+const OutputTitle = styled.summary``
+
 const Controls = styled.div`
   display: flex;
   flex-direction: column;
@@ -106,12 +119,21 @@ const Controls = styled.div`
     margin-top: 2rem;
   }
 `
+const DebugContainer = styled.div`
+  height: ${p => p.height * p.size}px;
+  width: ${p => p.width * p.size}px;
+  border: 1px solid hsl(0, 0%, 25%);
+  overflow: hidden;
+  position: relative;
+`
 
 const Debug = styled.div`
   height: ${p => p.height}px;
   width: ${p => p.width}px;
-  position: relative;
-  background: purple;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   &:after {
     content: '';
     height: 100%;
@@ -123,6 +145,208 @@ const Debug = styled.div`
     box-shadow: ${p => p.shadow};
   }
 `
+
+const PaletteContainer = styled.li`
+  height: auto !important;
+  border-left-color: ${p => p.color};
+`
+
+const Palette = styled.ul`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`
+
+const ColorSwatch = styled.button`
+  height: 44px;
+  width: 44px;
+  flex: 0 0 44px;
+  cursor: pointer;
+  background-color: ${p => p.color};
+  border: 2px solid ${p => (p.active ? 'white' : p.color)};
+  &:hover {
+    z-index: 2;
+    transform: scale(1.1);
+    transition: 0.15s ease 0s;
+  }
+`
+
+const Snapshots = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+`
+const Snapshot = styled.button`
+  height: 44px;
+  width: 44px;
+  flex: 0 0 44px;
+  padding: 0;
+  border: 0;
+  position: relative;
+  appearance: none;
+  cursor: pointer;
+
+  &:hover {
+    z-index: 2;
+    transform: scale(1.1);
+    transition: 0.15s ease 0s;
+  }
+
+  & > * {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform-origin: center center;
+    transform: translate(-50%, -50%) scale(${p => p.scale});
+  }
+`
+
+const SnapshotsContainer = styled.li`
+  height: auto !important;
+`
+
+const ControllerSnapshots = ({ snapshots, onChange, parent }) => {
+  if (!parent.current || !parent.current.domElement) return null
+  return createPortal(
+    <SnapshotsContainer className="cr snapshot-gallery">
+      <Snapshots>
+        {snapshots.map(snapshot => {
+          const {
+            created,
+            width,
+            height,
+            shadow,
+            size,
+            translateX,
+            translateY,
+            snapshotScale,
+          } = snapshot
+          return (
+            <Snapshot
+              onContextMenu={e => {
+                e.preventDefault()
+                return false
+              }}
+              onPointerDown={e => onChange(e, created, snapshot)}
+              scale={snapshotScale}
+              key={created}>
+              <DebugContainer width={width} height={height} size={size}>
+                <Debug
+                  shadow={shadow}
+                  width={size}
+                  height={size}
+                  translateX={translateX}
+                  translateY={translateY}
+                />
+              </DebugContainer>
+            </Snapshot>
+          )
+        })}
+      </Snapshots>
+    </SnapshotsContainer>,
+    parent.current.domElement.querySelector('ul')
+  )
+}
+ControllerSnapshots.propTypes = {
+  snapshots: T.arrayOf(T.shape({})),
+  onChange: T.func,
+}
+const ControllerPalette = ({ palette, color, onChange, parent }) => {
+  if (!parent.current || !parent.current.domElement) return null
+  return createPortal(
+    <PaletteContainer
+      className="cr color color-palette"
+      style={{ borderLeftColor: color }}>
+      <Palette>
+        {palette.map(c => (
+          <ColorSwatch
+            key={c}
+            active={color === c}
+            color={c}
+            onContextMenu={e => {
+              e.preventDefault()
+              return false
+            }}
+            onPointerDown={e => onChange(e, c)}
+          />
+        ))}
+      </Palette>
+    </PaletteContainer>,
+    parent.current.domElement.querySelector('ul')
+  )
+}
+ControllerPalette.propTypes = {
+  color: T.string,
+  palette: T.arrayOf(T.string),
+  onChange: T.func,
+}
+const ActionButton = styled.button`
+  background: transparent;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  border: 0;
+  text-align: left;
+  display: block;
+  color: #eee;
+  outline: transparent;
+`
+const Actions = ({ parent, onCss, onSvg, onSnapshot, onImage, onClear }) => {
+  if (!parent.current || !parent.current.domElement) return null
+  return createPortal(
+    <Fragment>
+      <li className="cr function">
+        <ActionButton
+          style={{ width: '100%' }}
+          onClick={onCss}
+          className="property-name">
+          Copy CSS
+        </ActionButton>
+      </li>
+      <li className="cr function">
+        <ActionButton
+          style={{ width: '100%' }}
+          onClick={onSvg}
+          className="property-name">
+          Save SVG
+        </ActionButton>
+      </li>
+      <li className="cr function">
+        <ActionButton
+          style={{ width: '100%' }}
+          onClick={onImage}
+          className="property-name">
+          Save PNG
+        </ActionButton>
+      </li>
+      <li className="cr function">
+        <ActionButton
+          style={{ width: '100%' }}
+          onClick={onSnapshot}
+          className="property-name">
+          Store snapshot
+        </ActionButton>
+      </li>
+      <li className="cr function">
+        <ActionButton
+          style={{ width: '100%' }}
+          onClick={onSnapshot}
+          className="property-name">
+          Clear canvas
+        </ActionButton>
+      </li>
+    </Fragment>,
+    parent.current.domElement.querySelector('ul')
+  )
+}
+Actions.propTypes = {
+  onClear: T.func,
+  onCss: T.func,
+  onSvg: T.func,
+  onImage: T.func,
+  onSnapshot: T.func,
+  parent: T.node,
+}
 // opacity: 0.5;
 
 const App = () => {
@@ -130,11 +354,134 @@ const App = () => {
   const [width, setWidth] = useState(CONFIG.width)
   const [height, setHeight] = useState(CONFIG.height)
   const [color, setColor] = useState(CONFIG.color)
+  const [debugging, setDebugging] = useState(CONFIG.debug)
+  // Purely as a placeholder to trigger a re-render
+  const [viewing, setViewing] = useState(false)
+  const [palette, setPalette] = useState(
+    window.localStorage.getItem('pixelator') &&
+      JSON.parse(window.localStorage.getItem('pixelator')).palette
+      ? [...JSON.parse(window.localStorage.getItem('pixelator')).palette]
+      : [CONFIG.color]
+  )
   const [shadow, setShadow] = useState('')
+  const [loadingSnapshot, setLoadingSnapshot] = useState(false)
+  const [snapshots, setSnapshots] = useState(
+    window.localStorage.getItem('pixelator') &&
+      JSON.parse(window.localStorage.getItem('pixelator')).snapshots
+      ? [...JSON.parse(window.localStorage.getItem('pixelator')).snapshots]
+      : []
+  )
+  const colorControllerRef = useRef(null)
+  const colorFolderRef = useRef(null)
+  const snapshotFolderRef = useRef(null)
+  const actionsFolderRef = useRef(null)
+  const settingsFolderRef = useRef(null)
   const [processing, setProcessing] = useState(false)
+  const [processingSnapshot, setProcessingSnapshot] = useState(false)
   const [translateX, setTranslateX] = useState(null)
   const [translateY, setTranslateY] = useState(null)
+  const snapshotRef = useRef(null)
   const cellRef = useRef([...new Array(height * width).fill().map(() => ({}))])
+
+  const deleteSnapshot = created => {
+    const newSnapshots = snapshots.filter(
+      snapshot => snapshot.created !== created
+    )
+    setSnapshots(newSnapshots)
+    window.localStorage.setItem(
+      'pixelator',
+      JSON.stringify({
+        snapshots: newSnapshots,
+        palette,
+      })
+    )
+  }
+
+  const handleSnapshot = (e, created, snapshot) => {
+    e.preventDefault()
+    if (e.button === 2) {
+      if (confirm('Are you sure you want to delete that snapshot?')) {
+        deleteSnapshot(created)
+      }
+    } else {
+      snapshotRef.current = snapshot
+      setLoadingSnapshot(true)
+    }
+  }
+
+  const deletePaletteColor = color => {
+    const newPalette = palette.filter(c => c !== color)
+    setPalette(newPalette)
+    window.localStorage.setItem(
+      'pixelator',
+      JSON.stringify({
+        snapshots,
+        palette: newPalette,
+      })
+    )
+  }
+
+  const onCss = () => setProcessing(true)
+  const onSvg = () => {}
+  const onClear = () => {}
+  const onSnapshot = () => {
+    generateShadow()
+    setProcessingSnapshot(true)
+  }
+  const onImage = () => {}
+
+  const onPaletteChange = (e, c) => {
+    e.preventDefault()
+    if (e.button === 2) {
+      if (
+        confirm('Are you sure you want to remove that color from the palette?')
+      ) {
+        deletePaletteColor(c)
+      }
+    } else {
+      setColor(c)
+    }
+  }
+
+  const generateShadow = useCallback(() => {
+    // Work out translation based on width, height, and size
+    // translateX half of the width plus half of the size
+    const translateX = width * size * 0.5 + size * 0.5
+    const translateY = height * size * 0.5 - size * 0.5
+    // generate the box shadow
+    // Iterate over the cell reference
+    let str = ``
+    for (let c = 0; c < cellRef.current.length; c++) {
+      const x = (c % width) + 1
+      const y = Math.floor(c / width)
+      // console.info(x, y)
+      if (cellRef.current[c].color) {
+        // Create a box shadow string and append it to the str
+        str += `${x * size}px ${y * size}px 0 0 ${cellRef.current[c].color},`
+      }
+    }
+    setTranslateX(translateX)
+    setTranslateY(translateY)
+    setShadow(
+      str.trim() === '' ? 'none' : str.substring(0, str.lastIndexOf(','))
+    )
+    setProcessing(false)
+  }, [height, size, width])
+
+  useEffect(() => {
+    if (loadingSnapshot) {
+      const { height, created, width, size, cells } = snapshotRef.current
+      cellRef.current = JSON.parse(cells)
+
+      setHeight(height)
+      setWidth(width)
+      setSize(size)
+      setViewing(created)
+      setLoadingSnapshot(false)
+    } else {
+      generateShadow()
+    }
+  }, [generateShadow, loadingSnapshot])
 
   useEffect(() => {
     const changeDimension = changer => dimension => {
@@ -151,49 +498,116 @@ const App = () => {
       }
     }
     const CONTROLLER = new GUI()
-    CONTROLLER.add(CONFIG, 'height', 0, 50, 1).onFinishChange(
-      changeDimension(setHeight)
-    )
-    CONTROLLER.add(CONFIG, 'width', 0, 50, 1).onFinishChange(
-      changeDimension(setWidth)
-    )
-    CONTROLLER.add(CONFIG, 'size', 0, 50, 1).onFinishChange(size => {
-      setSize(size)
-    })
-    CONTROLLER.addColor(CONFIG, 'color').onFinishChange(color =>
-      setColor(color)
-    )
+    const DIMENSIONS = CONTROLLER.addFolder('Dimensions')
+    DIMENSIONS.add(CONFIG, 'height', 0, 50, 1)
+      .onFinishChange(changeDimension(setHeight))
+      .name('Canvas height')
+    DIMENSIONS.add(CONFIG, 'width', 0, 50, 1)
+      .onFinishChange(changeDimension(setWidth))
+      .name('Canvas width')
+    DIMENSIONS.add(CONFIG, 'size', 0, 20, 1)
+      .onFinishChange(size => {
+        setSize(size)
+        // Will trigger shadow generation
+        setProcessing(true)
+      })
+      .name('Pixel size')
+
+    colorFolderRef.current = CONTROLLER.addFolder('Color')
+    colorControllerRef.current = colorFolderRef.current
+      .addColor(CONFIG, 'color')
+      .onFinishChange(color => {
+        setColor(color)
+      })
+      .name('Color')
+
+    snapshotFolderRef.current = CONTROLLER.addFolder('Snapshots')
+    settingsFolderRef.current = CONTROLLER.addFolder('Settings')
+    settingsFolderRef.current
+      .add(CONFIG, 'dark')
+      .onChange(value => {
+        // eslint-disable-next-line
+        console.info('DARK MODE', value)
+      })
+      .name('Dark mode')
+    settingsFolderRef.current
+      .add(CONFIG, 'debug')
+      .onChange(value => {
+        // eslint-disable-next-line
+        console.info('DEBUG MODE', value)
+        setDebugging(value)
+      })
+      .name('Show debug output')
+    // Add actions folder for buttons
+    actionsFolderRef.current = CONTROLLER.addFolder('Actions')
   }, [])
 
   useEffect(() => {
-    const generateShadow = () => {
-      // Work out translation based on width, height, and size
-      // translateX half of the width plus half of the size
-      const translateX = width * size * 0.5 + size * 0.5
-      const translateY = height * size * 0.5 - size * 0.5
-      // generate the box shadow
-      // Iterate over the cell reference
-      let str = ``
-      for (let c = 0; c < cellRef.current.length; c++) {
-        const x = (c % width) + 1
-        const y = Math.floor(c / width)
-        // console.info(x, y)
-        if (cellRef.current[c].color) {
-          // Create a box shadow string and append it to the str
-          str += `${x * size}px ${y * size}px 0 0 ${cellRef.current[c].color},`
-        }
-      }
-      setTranslateX(translateX)
-      setTranslateY(translateY)
-      setShadow(
-        str.trim() === '' ? 'none' : str.substring(0, str.lastIndexOf(','))
+    colorControllerRef.current.setValue(color)
+    if (palette.indexOf(color) === -1) {
+      window.localStorage.setItem(
+        'pixelator',
+        JSON.stringify({
+          palette: [...palette, color],
+          snapshots,
+        })
       )
-      setProcessing(false)
+      setPalette([...palette, color])
     }
+  }, [color, palette, snapshots])
+
+  useEffect(() => {
     if (processing) {
       generateShadow()
     }
-  }, [height, processing, size, width])
+  }, [generateShadow, height, processing, size, width])
+
+  useEffect(() => {
+    if (processingSnapshot) {
+      if (
+        snapshots.filter(snap => snap.cells === JSON.stringify(cellRef.current))
+          .length === 0
+      ) {
+        // Take all the current state, store it in localStorage.
+        // Work out a scale. Save it as a snapshot.
+        const SNAPSHOT = {
+          height,
+          width,
+          size,
+          color,
+          created: new Date().getTime(),
+          cells: JSON.stringify(cellRef.current),
+          translateX,
+          translateY,
+          shadow,
+          // Scale it down so it fits in a 44px button
+          snapshotScale: 44 / (Math.max(width, height) * size),
+        }
+
+        window.localStorage.setItem(
+          'pixelator',
+          JSON.stringify({
+            palette,
+            snapshots: [...snapshots, SNAPSHOT],
+          })
+        )
+        setSnapshots([...snapshots, SNAPSHOT])
+      }
+      setProcessingSnapshot(false)
+    }
+  }, [
+    color,
+    generateShadow,
+    height,
+    palette,
+    processingSnapshot,
+    shadow,
+    size,
+    snapshots,
+    translateX,
+    translateY,
+    width,
+  ])
 
   return (
     <Container>
@@ -203,20 +617,43 @@ const App = () => {
         height={height}
         cells={cellRef.current}
         color={color}
+        key={viewing}
       />
       <Controls>
-        <button onClick={() => setProcessing(true)}>
-          {processing ? 'Processing' : 'Print to console!'}
-        </button>
-
-        <Debug
-          shadow={shadow}
-          width={size}
-          height={size}
-          translateX={translateX}
-          translateY={translateY}
+        <ControllerPalette
+          color={color}
+          palette={palette}
+          parent={colorFolderRef}
+          onChange={onPaletteChange}
+        />
+        <ControllerSnapshots
+          snapshots={snapshots}
+          onChange={handleSnapshot}
+          parent={snapshotFolderRef}
+        />
+        <Actions
+          onCss={onCss}
+          onSvg={onSvg}
+          onSnapshot={onSnapshot}
+          onImage={onImage}
+          onClear={onClear}
+          parent={actionsFolderRef}
         />
       </Controls>
+      {debugging && (
+        <OutputDrawer>
+          <OutputTitle>Debugger</OutputTitle>
+          <DebugContainer width={width} height={height} size={size}>
+            <Debug
+              shadow={shadow}
+              width={size}
+              height={size}
+              translateX={translateX}
+              translateY={translateY}
+            />
+          </DebugContainer>
+        </OutputDrawer>
+      )}
     </Container>
   )
 }
