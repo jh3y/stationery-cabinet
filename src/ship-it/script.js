53aa3cfba38bd937e198f67b009b6b5fc300a4cc
jh3y <jh3y@users.noreply.github.com>
2020-07-25 05:17:50 +0100

    feat: get waves working for ship it button

--- src/ship-it/script.js
+++ src/ship-it/script.js
@@ -1,7 +1,9 @@
 const {
   gsap: {
-    // timeline,
+    timeline,
     set,
+    // to,
+    // fromTo,
     utils: { random },
   },
 } = window
@@ -22,6 +24,127 @@ set('.blank--15', { xPercent: -300 })
 set('.blank--16', { xPercent: -300, yPercent: -100 })
 set('.blank', { '--hue': () => random(0, 360) })
 set('.container', { '--hue': 0 })
+
+set(['.wave--1', '.wave--2'], { '--hue': random(180, 210) })
+set(['.wave--3', '.wave--4'], { '--hue': random(180, 210) })
+set('.boat__bubble', {
+  transformOrigin: '50% 50%',
+  '--lightness': () => random(50, 100),
+})
+
+set(['.wave--2', '.wave--4'], { xPercent: -100 })
+const WAVES_SET_ONE_DUR = random(0.8, 2)
+const WAVES_SET_TWO_DUR = random(0.8, 2)
+
+// const WAVES = timeline({ paused: true })
+timeline({ paused: true })
+  .to(
+    ['.wave--1', '.wave--2'],
+    {
+      repeat: -1,
+      xPercent: '+=100',
+      duration: WAVES_SET_ONE_DUR,
+      ease: 'none',
+    },
+    0
+  )
+  .to(
+    ['.wave--3', '.wave--4'],
+    {
+      repeat: -1,
+      xPercent: '+=100',
+      duration: WAVES_SET_TWO_DUR,
+      ease: 'none',
+    },
+    0
+  )
+  .to(
+    ['.wave--1', '.wave--2'],
+    {
+      repeat: -1,
+      duration: random(0.5, 2),
+      y: random(0, 5),
+      yoyo: true,
+      ease: 'none',
+    },
+    0
+  )
+  .to(
+    ['.wave--3', '.wave--4'],
+    {
+      repeat: -1,
+      duration: random(0.5, 2),
+      y: random(0, 5),
+      yoyo: true,
+      ease: 'none',
+    },
+    0
+  )
+
+// const STEAM = timeline({ paused: true })
+timeline({ paused: true })
+  .fromTo(
+    '.boat__bubble',
+    {
+      x: 0,
+      y: 0,
+      scale: 0,
+    },
+    {
+      stagger: {
+        each: 0.15,
+        repeat: -1,
+      },
+      repeatRefresh: true,
+      x: () => random(5, 25),
+      y: () => random(-5, -15),
+      scale: () => random(1, 2.5),
+      duration: () => random(0.1, 1.5),
+      opacity: 0,
+    }
+  )
+  .to('.boat__bubble', { opacity: 0 }, '-=0.0015')
+
+const EYES = document.querySelector('.boat__eyes')
+const blink = EYES => {
+  set(EYES, { scaleY: 1 })
+  if (EYES.BLINK_TL) EYES.BLINK_TL.kill()
+  EYES.BLINK_TL = timeline({
+    delay: Math.floor(Math.random() * 4) + 1,
+    onComplete: () => blink(EYES),
+  })
+  EYES.BLINK_TL.to(EYES, {
+    duration: 0.05,
+    transformOrigin: '50% 50%',
+    scaleY: 0,
+    yoyo: true,
+    repeat: 1,
+  })
+}
+blink(EYES)
+
+// const blast = () => {
+//   const newBubble = document.querySelector('.boat__bubble').cloneNode()
+//   document.querySelector('.boat__bubbles').appendChild(newBubble)
+//   to(newBubble, {
+//     scale: 2,
+//     duration: 0.5,
+//     onComplete: () => newBubble.remove()
+//   })
+//   requestAnimationFrame(blast)
+// }
+
+// .to(
+//   ['.wave--1', '.wave--2'],
+//   {
+//     duration: random(0.5, 2),
+//     yPercent: random(-10, 0),
+//     repeat: -1,
+//     yoyo: true,
+//     ease: 'none'
+//   },
+//   0
+// )
 // set('.blank--two', { xPercent: -100 })
 
 // set('.ship-it', {
