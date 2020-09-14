19fe49aa5353223a4cb76b6f1ef466b489ed8815
jh3y <jh3y@users.noreply.github.com>
2020-09-14 19:36:02 +0100

    feat: tada

--- src/100-divs-confetti/script.js
+++ src/100-divs-confetti/script.js
@@ -1,42 +1,106 @@
 const { gsap } = window
+const SIZE_LOWER = 0.75
+const SIZE_UPPER = 2.5
+gsap.set('.confetti', {
+  '--rotate': () => gsap.utils.random(-45, 45),
+  '--hue': () => gsap.utils.random(0, 359),
+  opacity: 0,
+})
+gsap.set('.confetti__content', {
+  '--width': () => gsap.utils.random(SIZE_LOWER, SIZE_UPPER),
+  '--height': () => gsap.utils.random(SIZE_LOWER, SIZE_UPPER),
+  rotate: -60,
+  xPercent: -50,
+  yPercent: -50,
+})
 
-gsap.set('.confetti__wrapper', { '--rotate': () => gsap.utils.random(-30, 30) })
-gsap.set('.confetti', { '--hue': () => gsap.utils.random(0, 359) })
-gsap
-  .timeline({
-    repeat: -1,
-  })
-  .set('.confetti__content', {
-    rotate: -60,
-    rotateX: 0,
-    rotateY: 0,
-    rotateZ: 0,
-    opacity: 1,
-  })
-  .to('.confetti', {
-    duration: () => gsap.utils.random(2, 4),
-    physics2D: {
-      velocity: () => gsap.utils.random(100, 130),
-      angle: -60,
-      acceleration: 50,
-      accelerationAngle: 90,
+const CONFETTI = document.querySelectorAll('.confetti')
+const STREAMER = new Audio('https://assets.codepen.io/605876/horn.mp3')
+const genConfettiTL = () => {
+  const TL = gsap.timeline({
+    onStart: () => {
+      STREAMER.currentTime = 0
+      STREAMER.play()
     },
   })
-  .to(
-    '.confetti__content',
-    {
-      rotateX: () => gsap.utils.random(0, 360),
-      rotateY: () => gsap.utils.random(0, 360),
-      rotateZ: () => gsap.utils.random(0, 360),
-      duration: () => gsap.utils.random(1, 4),
-    },
-    '<'
-  )
-// .to(
-//   '.confetti__content',
-//   {
-//     opacity: 0,
-//     duration: 0.2,
-//   },
-//   '>-0.2'
-// )
+  const VMIN = Math.min(window.innerHeight, window.innerWidth) / 100
+  CONFETTI.forEach(piece => {
+    const duration = gsap.utils.random(0.5, 3)
+    const CONFETTI_TL = gsap
+      .timeline()
+      .to(piece, {
+        opacity: 1,
+        duration: duration * 0.25,
+      })
+      .to(
+        piece,
+        {
+          ease: 'power3.out',
+          duration,
+          physics2D: {
+            velocity: gsap.utils.random(25 * VMIN, 60 * VMIN),
+            angle: gsap.utils.random(-80, -40),
+            gravity: gsap.utils.random(25 * VMIN, 60 * VMIN),
+          },
+        },
+        0
+      )
+      .to(
+        piece.children,
+        {
+          rotateX: () => gsap.utils.random(0, 1080),
+          rotateY: () => gsap.utils.random(0, 1080),
+          rotateZ: () => gsap.utils.random(0, 1080),
+          duration,
+        },
+        '<'
+      )
+      .to(
+        piece,
+        {
+          opacity: 0,
+          duration: duration * 0.25,
+        },
+        `>-${duration * 0.25}`
+      )
+    TL.add(CONFETTI_TL, 0)
+  })
+  return TL
+}
+
+const FIRE = () => {
+  gsap.set('.confetti', {
+    '--rotate': () => gsap.utils.random(-30, 30),
+    '--hue': () => gsap.utils.random(0, 359),
+  })
+  gsap.set('.confetti__content', {
+    '--width': () => gsap.utils.random(SIZE_LOWER, SIZE_UPPER),
+    '--height': () => gsap.utils.random(SIZE_LOWER, SIZE_UPPER),
+  })
+  gsap
+    .timeline()
+    .set('.confetti', { opacity: 1, x: 0, y: 0 })
+    .set('.confetti__content', {
+      rotate: -60,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+      opacity: 1,
+    })
+    .to('.confetti__gun', {
+      '--z': 0,
+      duration: 0.1,
+    })
+    .add(genConfettiTL(), 0)
+}
+
+document.body.addEventListener('pointerup', () => {
+  FIRE()
+})
+
+document.body.addEventListener('pointerdown', () => {
+  gsap.to('.confetti__gun', {
+    '--z': 20,
+    duration: 0.1,
+  })
+})
