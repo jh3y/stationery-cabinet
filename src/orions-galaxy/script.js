5874f7970db5b8c44eb55a013af0a081a2db28ee
jh3y <jh3y@users.noreply.github.com>
2020-09-14 12:03:07 +0100

    feat: orions galaxy

--- src/orions-galaxy/script.js
+++ src/orions-galaxy/script.js
@@ -10,6 +10,40 @@ const {
 let ZOOM
 let zoomed = false
 
+const AUDIO = {
+  AMBIENCE: new Audio('https://assets.codepen.io/605876/street-ambience.mp3'),
+  MEW: new Audio('https://assets.codepen.io/605876/kitty-mew.mp3'),
+  MEWTWO: new Audio('https://assets.codepen.io/605876/kitty-mew-2.mp3'),
+  PURRING: new Audio('https://assets.codepen.io/605876/kitty-purring.mp3'),
+  ROCK: new Audio(
+    'https://assets.codepen.io/605876/Monplaisir-Aimer_cest_ce_quil_y_a_de_plus_beau.mp3'
+  ),
+}
+
+AUDIO.ROCK.loop = AUDIO.AMBIENCE.loop = AUDIO.PURRING.loop = true
+AUDIO.ROCK.volume = 0
+AUDIO.ROCK.muted = AUDIO.AMBIENCE.muted = AUDIO.MEW.muted = AUDIO.MEWTWO.muted = AUDIO.PURRING.muted = true
+
+const toggleAudio = () => {
+  if (AUDIO.ROCK.currentTime === 0 || AUDIO.AMBIENCE.currentTime === 0) {
+    AUDIO.ROCK.play()
+    AUDIO.AMBIENCE.play()
+    AUDIO.PURRING.play()
+  }
+  AUDIO.ROCK.muted = AUDIO.AMBIENCE.muted = AUDIO.MEW.muted = AUDIO.MEWTWO.muted = AUDIO.PURRING.muted = !AUDIO
+    .PURRING.muted
+}
+
+document.querySelector('#volume').addEventListener('input', toggleAudio)
+
+const MEW = () => {
+  setTimeout(() => {
+    AUDIO[Math.random() > 0.5 ? 'MEW' : 'MEWTWO'].play()
+    MEW()
+  }, random(1000, 20000))
+}
+MEW()
+
 const ORION = document.querySelector('.orion')
 const TURTLE = document.querySelector('.turtle')
 const OBJECTS = [
@@ -35,6 +69,14 @@ set('.orion__eye', { transformOrigin: '50% 50%', rotate: -10 })
 set('.turtle__tail', { transformOrigin: '0% 50%' })
 set('.turtle__eye', { transformOrigin: '50% 50%', scale: 0.8 })
 set('.turtle__head', { transformOrigin: '80% 80%', rotate: 5 })
+set('.logo', { opacity: 0.75 })
+const SPACE_BLOBS = document.querySelectorAll('.space-blob')
+set(SPACE_BLOBS[6], {
+  yPercent: -25,
+  scale: 1.5,
+  xPercent: -25,
+  transformOrigin: '50% 50%',
+})
 
 const getScale = () => {
   // At this point work out the bigger of viewport dimensions
@@ -55,24 +97,36 @@ const getZoom = () =>
     .set(ORION, {
       scale: 1,
       attr: {
-        viewBox: '0 -200 5246.15 7862.26',
+        viewBox: '-200 -200 5646.15 7862.26',
       },
     })
     .to(ORION, {
-      ease: 'none',
       scale: getScale(),
-      duration: 0.25,
+      duration: 0.5,
     })
     .to(
       ORION,
       {
-        ease: 'none',
         attr: {
           viewBox: '3146.446 4215.751 56.335 6.775',
         },
         duration: 1,
       },
       0
+    )
+    .to(
+      AUDIO.ROCK,
+      {
+        volume: 1,
+      },
+      '>-0.5'
+    )
+    .to(
+      [AUDIO.AMBIENCE, AUDIO.MEW, AUDIO.MEWTWO, AUDIO.PURRING],
+      {
+        volume: 0,
+      },
+      '<'
     )
     .to(TURTLE, {
       opacity: 1,
@@ -85,7 +139,7 @@ const RESET = () => {
     ZOOM.pause()
     ZOOM.time(0)
   }
-  set(ORION, { attr: { viewBox: '0 -200 5246.15 7862.26' } })
+  set(ORION, { attr: { viewBox: '-200 -200 5646.15 7862.26' } })
   ORION.removeAttribute('style')
   set(ORION, { opacity: 1 })
   zoomed = false
@@ -94,18 +148,19 @@ const RESET = () => {
 RESET()
 
 // General mood timelines
-to('.orion__head', {
-  rotate: 'random(-5, 5)',
-  duration: random(1, 2),
-  repeatRefresh: true,
-  repeat: -1,
-  delay: random(0, 2),
-  repeatDelay: 5,
-})
+const TILT = () => {
+  to('.orion__head', {
+    rotate: 'random(-10, 10)',
+    duration: random(0.5, 2),
+    delay: random(0, 2),
+    onComplete: () => TILT(),
+  })
+}
+TILT()
 
 const WAG = () => {
-  timeline().to('.orion__tail', {
-    delay: random(2, 6),
+  to('.orion__tail', {
+    delay: random(1, 4),
     duration: random(0.25, 2),
     rotate: random(0, 10),
     onComplete: () => WAG(),
@@ -127,9 +182,10 @@ const BLINK = () => {
 BLINK()
 
 OBJECTS.forEach(OBJ => {
+  const duration = random(1, 4)
   timeline({ repeat: -1 })
     .to(OBJ, {
-      duration: 2,
+      duration: duration,
       ease: 'none',
       x: 80,
       delay: () => random(0, 10),
@@ -139,8 +195,9 @@ OBJECTS.forEach(OBJ => {
       {
         opacity: 1,
         repeat: 1,
+        repeatDelay: duration - 1,
         yoyo: true,
-        duration: 1,
+        duration: 0.5,
       },
       '<'
     )
@@ -160,20 +217,22 @@ to('.turtle__flipper', {
 })
 to('.turtle__head', {
   repeat: -1,
-  duration: 2,
+  duration: 1.5,
   ease: 'sine.inOut',
   yoyo: true,
   rotate: -5,
 })
 
-document.body.addEventListener('click', () => {
+const handleZoom = () => {
   if (ZOOM && ZOOM.progress() !== 1 && ZOOM.progress() !== 0) return
   if (zoomed && ZOOM) ZOOM.reverse()
   else {
     ZOOM = getZoom()
     ZOOM.play()
   }
-})
+}
+ORION.addEventListener('click', handleZoom)
+TURTLE.addEventListener('click', handleZoom)
 
 // On Resize, reset to non-zoomed for ease.
 window.addEventListener('resize', RESET)
