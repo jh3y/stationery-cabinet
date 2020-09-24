55b57aeb08ac3a02a57893148c7c807655e0374a
jh3y <jh3y@users.noreply.github.com>
2020-09-24 20:41:52 +0100

    feat: check in windows for room pre plant pot and chair

--- src/3d-studio/script.js
+++ src/3d-studio/script.js
@@ -7,11 +7,13 @@ const CONTROLLER = new GUI()
 const CONFIG = {
   'rotate-x': -20,
   'rotate-y': -45,
+  dark: false,
 }
 const UPDATE = () => {
   Object.entries(CONFIG).forEach(([key, value]) => {
     document.documentElement.style.setProperty(`--${key}`, value)
   })
+  document.documentElement.style.setProperty('--dark', CONFIG.dark ? 1 : 0)
 }
 const PLANE_FOLDER = CONTROLLER.addFolder('Plane')
 PLANE_FOLDER.add(CONFIG, 'rotate-x', -360, 360, 1)
@@ -19,5 +21,10 @@ PLANE_FOLDER.add(CONFIG, 'rotate-x', -360, 360, 1)
   .onChange(UPDATE)
 PLANE_FOLDER.add(CONFIG, 'rotate-y', -360, 360, 1)
   .name('Rotate Y (deg)')
+  .onChange(UPDATE)
+
+const ACTIONS_FOLDER = CONTROLLER.addFolder('Actions')
+ACTIONS_FOLDER.add(CONFIG, 'dark')
+  .name('Dark mode?')
   .onChange(UPDATE)
 UPDATE()
