411cc27a926e9d51b2562a6bb9bb8679a7d7aa8b
jh3y <jh3y@users.noreply.github.com>
2020-09-25 22:08:33 +0100

    feat: check in shading foundation

--- src/3d-studio/script.js
+++ src/3d-studio/script.js
@@ -8,12 +8,16 @@ const CONFIG = {
   'rotate-x': -20,
   'rotate-y': -45,
   dark: false,
+  zoomed: false,
+  spin: false,
 }
 const UPDATE = () => {
   Object.entries(CONFIG).forEach(([key, value]) => {
     document.documentElement.style.setProperty(`--${key}`, value)
   })
   document.documentElement.style.setProperty('--dark', CONFIG.dark ? 1 : 0)
+  document.documentElement.style.setProperty('--zoomed', CONFIG.zoomed ? 1 : 0)
+  document.documentElement.style.setProperty('--spin', CONFIG.spin ? 1 : 0)
 }
 const PLANE_FOLDER = CONTROLLER.addFolder('Plane')
 PLANE_FOLDER.add(CONFIG, 'rotate-x', -360, 360, 1)
@@ -26,5 +30,11 @@ PLANE_FOLDER.add(CONFIG, 'rotate-y', -360, 360, 1)
 const ACTIONS_FOLDER = CONTROLLER.addFolder('Actions')
 ACTIONS_FOLDER.add(CONFIG, 'dark')
   .name('Dark mode?')
+  .onChange(UPDATE)
+ACTIONS_FOLDER.add(CONFIG, 'zoomed')
+  .name('Zoom?')
+  .onChange(UPDATE)
+ACTIONS_FOLDER.add(CONFIG, 'spin')
+  .name('Spin?')
   .onChange(UPDATE)
 UPDATE()
