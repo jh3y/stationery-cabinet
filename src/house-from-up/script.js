341561a938afb10303c7206295a3c99ffad8fe40
jh3y <jh3y@users.noreply.github.com>
2020-10-02 15:47:45 +0100

    feat: house from up minus animation

--- src/house-from-up/script.js
+++ src/house-from-up/script.js
@@ -1,1 +1,19 @@
-// 404
+// Purely for debugging purposes
+const {
+  dat: { GUI },
+} = window
+
+const CONTROLLER = new GUI()
+const CONFIG = {
+  'rotate-y': -55,
+}
+const UPDATE = () => {
+  Object.entries(CONFIG).forEach(([key, value]) => {
+    document.documentElement.style.setProperty(`--${key}`, value)
+  })
+}
+const PLANE_FOLDER = CONTROLLER.addFolder('Plane')
+PLANE_FOLDER.add(CONFIG, 'rotate-y', -360, 360, 1)
+  .name('Rotate Y (deg)')
+  .onChange(UPDATE)
+UPDATE()
