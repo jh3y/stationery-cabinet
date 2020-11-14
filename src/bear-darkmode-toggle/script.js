f1683c02f0134aad6c3719112e8e1da60af34b9d
jh3y <jh3y@users.noreply.github.com>
2020-11-14 19:08:02 +0000

    feat: light bulb toggle v1

---
+++ src/bear-darkmode-toggle/script.js
@@ -0,0 +1,97 @@
+const {
+  gsap: { registerPlugin, set, to, timeline },
+  MorphSVGPlugin,
+  Draggable,
+} = window
+registerPlugin(MorphSVGPlugin)
+
+// Used to calculate distance of "tug"
+let startX
+let startY
+let endX
+let endY
+
+const AUDIO = {
+  CLICK: new Audio('https://assets.codepen.io/605876/click.mp3'),
+}
+const STATE = {
+  ON: false,
+}
+const CORD_DURATION = 0.1
+
+const CORDS = document.querySelectorAll('.toggle-scene__cord')
+const HIT = document.querySelector('.toggle-scene__hit-spot')
+const DUMMY = document.querySelector('.toggle-scene__dummy-cord')
+const DUMMY_CORD = document.querySelector('.toggle-scene__dummy-cord line')
+const PROXY = document.createElement('div')
+// set init position
+const RESET = () => {
+  set(PROXY, {
+    x: DUMMY_CORD.getAttribute('x2'),
+    y: DUMMY_CORD.getAttribute('y2'),
+  })
+}
+
+RESET()
+
+const CORD_TL = timeline({
+  paused: true,
+  onStart: () => {
+    STATE.ON = !STATE.ON
+    set(document.documentElement, { '--on': STATE.ON ? 1 : 0 })
+    set(DUMMY, { display: 'none' })
+    set(CORDS[0], { display: 'block' })
+    AUDIO.CLICK.play()
+  },
+  onComplete: () => {
+    set(DUMMY, { display: 'block' })
+    set(CORDS[0], { display: 'none' })
+    RESET()
+  },
+})
+
+for (let i = 1; i < CORDS.length; i++) {
+  CORD_TL.add(
+    to(CORDS[0], {
+      morphSVG: CORDS[i],
+      duration: CORD_DURATION,
+      repeat: 1,
+      yoyo: true,
+    })
+  )
+}
+
+Draggable.create(PROXY, {
+  trigger: HIT,
+  type: 'x,y',
+  onPress: e => {
+    startX = e.x
+    startY = e.y
+    endX = DUMMY_CORD.getAttribute('x2')
+    endY = DUMMY_CORD.getAttribute('y2')
+  },
+  onDrag: function() {
+    set(DUMMY_CORD, {
+      attr: {
+        x2: this.x,
+        y2: this.y,
+      },
+    })
+  },
+  onRelease: function(e) {
+    const DISTX = Math.abs(e.x - startX)
+    const DISTY = Math.abs(e.y - startY)
+    const TRAVELLED = Math.sqrt(DISTX * DISTX + DISTY * DISTY)
+    to(DUMMY_CORD, {
+      attr: { x2: endX, y2: endY },
+      duration: CORD_DURATION,
+      onComplete: () => {
+        if (TRAVELLED > 50) {
+          CORD_TL.restart()
+        } else {
+          RESET()
+        }
+      },
+    })
+  },
+})
