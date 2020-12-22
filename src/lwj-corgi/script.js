d60583c0c339bd3b72f724cbe64b57dd99c794cc
jh3y <jh3y@users.noreply.github.com>
2020-12-22 13:35:43 +0000

    feat: updates

--- src/lwj-corgi/script.js
+++ src/lwj-corgi/script.js
@@ -1,10 +1,75 @@
 import gsap from 'https://cdn.skypack.dev/gsap'
 
-const HAT_TOP = document.querySelector('.corgi__hat-top')
-const CRACKER = document.querySelector('.corgi__cracker')
-const TONGUE = document.querySelector('.corgi__tongue')
-gsap.set(CRACKER, { display: 'none' })
-gsap.set(TONGUE, { transformOrigin: '50% 0', scaleY: 0.85 })
-gsap.set(HAT_TOP, { transformOrigin: '70% 10%' })
-gsap.to(HAT_TOP, { repeat: -1, duration: 0.5, rotate: 20, yoyo: true })
-gsap.to(TONGUE, { repeat: -1, duration: 0.5, scaleY: 1.5, yoyo: true })
+gsap.set('.party-set g:nth-of-type(2)', { yPercent: 100 })
+gsap.set('.party-set', {
+  rotate: -45,
+  transformOrigin: '50% 50%',
+  xPercent: 50,
+})
+gsap.to('.party-set g', {
+  yPercent: '-=100',
+  repeat: -1,
+  duration: 2,
+  ease: 'none',
+})
+// // Speech Recognition stuff
+
+// window.SpeechRecognition =
+//   window.SpeechRecognition || window.webkitSpeechRecognition
+
+// const RECOG = new window.SpeechRecognition()
+// RECOG.lang = 'en-US'
+// // Continous results or not
+// RECOG.continuous = true
+// RECOG.interimResults = false
+
+// const STATE = {
+//   RUNNING: false,
+//   ACTIVATED: false,
+//   ROTATE: 0,
+// }
+
+// const START = () => {
+//   if (!STATE.RUNNING) {
+//     STATE.RUNNING = true
+//     document.body.style.backgroundColor = 'hsl(210, 50%, 50%)'
+//     RECOG.start()
+//   }
+// }
+
+// const COMMANDS = {
+//   PARTY: 'party',
+// }
+// const LISTEN_FOR_ACTIVATION = res => {
+//   const TRANSCRIPT = res.results[res.results.length - 1][0].transcript
+//     .toLowerCase()
+//     .trim()
+//   console.info(TRANSCRIPT)
+//   if (STATE.ACTIVATED) {
+//     if (TRANSCRIPT === COMMANDS.PARTY) {
+//       PARTY.play()
+//     }
+//   }
+//   // Listen for a keyword that we can do something with??
+//   else if (TRANSCRIPT.trim() === KEYWORD) {
+//     // Go into active mode
+//     // On the fly change the listener???
+//     STATE.ACTIVATED = true
+//     document.body.style.backgroundColor = 'hsl(120, 50%, 50%)'
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = 'hsl(210, 50%, 50%)'
+//       STATE.ACTIVATED = false
+//       PARTY.pause()
+//     }, 10000)
+//   }
+// }
+
+// const KEYWORD = 'corgi'
+// RECOG.onresult = LISTEN_FOR_ACTIVATION
+// RECOG.onend = () => {
+//   console.info('restarting')
+//   // Shouldn't end, restart.
+//   // Only restart if state is still running.
+//   if (STATE.RUNNING) RECOG.start()
+// }
+// START()
