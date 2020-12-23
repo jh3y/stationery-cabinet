d1fa87be1caaa07c6b53d8c8dbe48bae46768a9f
jh3y <jh3y@users.noreply.github.com>
2020-12-23 23:00:11 +0000

    feat: feed the world foundation

--- src/feed-the-world/script.js
+++ src/feed-the-world/script.js
@@ -1,3 +1,51 @@
+import gsap from 'https://cdn.skypack.dev/gsap'
+
+const CHUNK = 1000
+
+gsap.set(['.globe__mouth', '.globe__eyes'], { transformOrigin: '50% 50%' })
+const UPDATE_FEATURES = ({ x, y }) => {
+  const newX = gsap.utils.mapRange(0, window.innerWidth, -5, 5, x)
+  const newY = gsap.utils.mapRange(0, window.innerHeight, -5, 5, y)
+  gsap.set('.globe__face', {
+    x: newX,
+    y: newY,
+  })
+}
+
+const BLINK = () => {
+  const delay = gsap.utils.random(1, 5)
+  gsap.to('.globe__eyes', {
+    delay,
+    scaleY: 0.1,
+    repeat: 3,
+    yoyo: true,
+    duration: 0.05,
+    onComplete: () => {
+      BLINK()
+    },
+  })
+}
+BLINK()
+window.addEventListener('pointermove', UPDATE_FEATURES)
+
+const POPULATE_FOOD = () => {
+  const ITEM = document.createElement('div')
+  ITEM.className = 'food'
+  ITEM.style.setProperty(
+    '--x',
+    gsap.utils.random(window.innerWidth * 0.25, window.innerWidth * 0.75)
+  )
+  ITEM.style.setProperty(
+    '--y',
+    gsap.utils.random(window.innerHeight * 0.25, window.innerHeight * 0.75)
+  )
+  document.body.appendChild(ITEM)
+}
+for (let i = 0; i < 5; i++) {
+  POPULATE_FOOD()
+}
+// Start YouTube Embed Code
+
 // YouTube Video ID
 const videoId = '8NLLAeNhH3k'
 
@@ -28,7 +76,7 @@ const UPDATE = () => {
 const topUp = () => {
   STATE.PLAYING = true
   window.PLAYER.playVideo()
-  STATE.TIMER += 1000
+  STATE.TIMER += CHUNK
   TIMER_LABEL.innerText = STATE.TIMER
   if (STATE.TIMEOUT) clearTimeout(STATE.TIMEOUT)
   STATE.TIMEOUT = setTimeout(shutOff, STATE.TIMER)
@@ -39,7 +87,7 @@ const topUp = () => {
 const onReady = () => {
   // Mute the player by default/for streaming
   // TODO: Remove!
-  window.PLAYER.mute()
+  // window.PLAYER.mute()
   // seekTo can skip to miss the gap at the start
   window.PLAYER.seekTo(10)
   // Pause it as seek starts playing
@@ -55,14 +103,16 @@ const onStateChange = e => {
     STATE.PLAYING = false
   }
 }
-window.onYouTubeIframeAPIReady = () => {
-  window.PLAYER = new window.YT.Player('player', {
-    height: 200,
-    width: 200,
-    videoId,
-    events: {
-      onReady,
-      onStateChange,
-    },
-  })
-}
+// console.info(YT)
+// window.onYouTubeIframeAPIReady = () => {
+// console.info('hello')
+window.PLAYER = new window.YT.Player('player', {
+  height: 200,
+  width: 200,
+  videoId,
+  events: {
+    onReady,
+    onStateChange,
+  },
+})
+// }
