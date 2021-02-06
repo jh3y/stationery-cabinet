73bb30d25116c0b8a20b0aee25b775052fe7f363
jh3y <jh3y@users.noreply.github.com>
2021-02-06 21:41:03 +0000

    feat: bear paw button progress

--- src/bear-paw-button/script.js
+++ src/bear-paw-button/script.js
@@ -1,43 +1,160 @@
 import gsap from 'https://cdn.skypack.dev/gsap'
 const { timeline } = gsap
+const TRANSFORMER = document.querySelector('.transformer')
+const TTEXT = document.querySelector('.transformer__middle')
+const duration = 0.2
 
-const LIKE_TL = timeline({
-  paused: true,
-  onStart: () => {
-    // Ensure hover is back to 0
-    gsap.set(['.like__feature', '.like__text--traveller'], { '--hovered': 0 })
-  },
-  onReverseComplete: () => {
-    // Ensure we remove the set hover.
-    gsap.set(['.like__feature', '.like__text--traveller'], {
-      clearProps: 'all',
-    })
-  },
+/**
+ * The Timeline
+ * 1. Move Paw to Left
+ * 2. Meet to slap in the middle with right paw
+ * 3. Paws disappear
+ */
+
+const LEFT_PAW_TL = timeline()
+  .to('.paw--left', {
+    xPercent: -100,
+    duration: 0.5,
+  })
+  .to('.paw--left', {
+    xPercent: 20,
+    duration: 0.5,
+  })
+  .to('.paw--left', {
+    xPercent: -50,
+    duration: 0.5,
+  })
+
+const RIGHT_PAW_TL = timeline().to('.paw--right', {
+  xPercent: 10,
+  yPercent: 0,
+  repeat: 1,
+  yoyo: true,
+  duration: 0.5,
 })
-  .to('.like__text--spacer', {
-    width: 0,
-    margin: 0,
-    duration: 0.2,
+
+gsap.set('.paw--right', {
+  yPercent: 175,
+  xPercent: 15,
+})
+
+gsap.set('.number--next', {
+  yPercent: -100,
+  opacity: 0,
+})
+
+const RESIZE_TL = timeline()
+  .set('.transformer__feature', {
+    xPercent: -100,
   })
   .to(
-    '.like__text--traveller',
+    '.transformer__outline',
     {
-      duration: 0.2,
-      opacity: 0,
+      scaleX: 0.6,
+      duration,
+    },
+    0
+  )
+  .to(
+    '.transformer__front',
+    {
+      x: TTEXT.offsetWidth / 2,
+      duration,
+    },
+    0
+  )
+  .to(
+    '.transformer__back',
+    {
+      x: -(TTEXT.offsetWidth / 2),
+      duration,
+    },
+    0
+  )
+  .to(
+    '.number.number--current.number--single',
+    {
       yPercent: 100,
+      opacity: 0,
+      duration,
+    },
+    0
+  )
+  .to(
+    '.number.number--next.number--single',
+    {
+      yPercent: 0,
+      duration,
+      opacity: 1,
     },
     0
   )
 
-const BUTTON = document.querySelector('button')
-BUTTON.addEventListener('click', () => {
-  if (LIKE_TL.progress() === 1) {
-    gsap.set(['.like__feature', '.like__text--traveller'], {
-      clearProps: 'all',
-    })
-    LIKE_TL.reverse()
+const TRANSFORMER_TL = timeline({
+  paused: true,
+})
+  .set('.transformer__feature', {
+    xPercent: 0,
+  })
+  .set('.transformer__text', {
+    yPercent: 100,
+    opacity: 0,
+  })
+  .add(LEFT_PAW_TL)
+  .add(RIGHT_PAW_TL, 0.5)
+  .add(RESIZE_TL)
+
+const RESET_TL = timeline({
+  paused: true,
+})
+  .set(['.transformer__feature', '.transformer__text'], {
+    clearProps: 'all',
+  })
+  .to('.transformer__front', {
+    x: 0,
+    duration,
+  })
+  .to(
+    '.transformer__outline',
+    {
+      scaleX: 1,
+      duration,
+    },
+    0
+  )
+  .to(
+    '.transformer__back',
+    {
+      x: 0,
+      duration,
+    },
+    0
+  )
+  .to(
+    '.number.number--current.number--single',
+    {
+      yPercent: 0,
+      duration,
+      opacity: 1,
+    },
+    0
+  )
+  .to(
+    '.number.number--next.number--single',
+    {
+      yPercent: -100,
+      duration,
+      opacity: 0,
+    },
+    0
+  )
+
+TRANSFORMER.addEventListener('click', () => {
+  if (TRANSFORMER.getAttribute('aria-pressed') === 'true') {
+    TRANSFORMER.setAttribute('aria-pressed', false)
+    RESET_TL.restart()
   } else {
-    LIKE_TL.restart()
-    BUTTON.setAttribute('aria-pressed', true)
+    TRANSFORMER_TL.restart()
+    TRANSFORMER.setAttribute('aria-pressed', true)
   }
 })
