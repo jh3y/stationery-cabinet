60bb26fab6c1aa00a2882272a6406d344f4bac72
jh3y <jh3y@users.noreply.github.com>
2021-02-07 20:37:58 +0000

    feat: progress on bear paw button

--- src/bear-paw-button/script.js
+++ src/bear-paw-button/script.js
@@ -1,8 +1,23 @@
-import gsap from 'https://cdn.skypack.dev/gsap'
+// import gsap from 'https://cdn.skypack.dev/gsap'
+const gsap = window.gsap
 const { timeline } = gsap
 const TRANSFORMER = document.querySelector('.transformer')
 const TTEXT = document.querySelector('.transformer__middle')
 const duration = 0.2
+
+gsap.set('.number--next', {
+  yPercent: -100,
+  opacity: 0,
+})
+gsap.set('.slap__line', {
+  transformOrigin: '50% 50%',
+  rotate: index => -45 + index * 45,
+})
+gsap.set('.slap__line path', {
+  transformOrigin: '50% 50%',
+  yPercent: -350,
+  scaleY: 0.75,
+})
 
 /**
  * The Timeline
@@ -11,103 +26,244 @@ const duration = 0.2
  * 3. Paws disappear
  */
 
-const LEFT_PAW_TL = timeline()
-  .to('.paw--left', {
-    xPercent: -100,
-    duration: 0.5,
-  })
-  .to('.paw--left', {
-    xPercent: 20,
-    duration: 0.5,
-  })
-  .to('.paw--left', {
-    xPercent: -50,
-    duration: 0.5,
+const LEFT_PAW_TL = () =>
+  timeline({})
+    .set('.transformer__feature', {
+      transition: 'none',
+    })
+    .to('.transformer__feature', {
+      keyframes: [
+        {
+          xPercent: -130,
+          ease: 'power1.out',
+          duration: 0.125,
+        },
+        {
+          xPercent: 20,
+          ease: 'power1.in',
+          duration: 0.125,
+        },
+        {
+          xPercent: -20,
+          delay: 0.1,
+          ease: 'power1.out',
+          duration: 0.15,
+        },
+      ],
+    })
+
+const RIGHT_PAW_TL = () =>
+  timeline().to('.paw--right', {
+    keyframes: [
+      {
+        ease: 'power1.in',
+        xPercent: -175,
+        yPercent: -125,
+        duration: 0.25,
+      },
+      {
+        ease: 'power1.out',
+        delay: 0.1,
+        duration: 0.15,
+        xPercent: 0,
+        yPercent: 0,
+      },
+    ],
   })
 
-const RIGHT_PAW_TL = timeline().to('.paw--right', {
-  xPercent: 10,
-  yPercent: 0,
-  repeat: 1,
-  yoyo: true,
-  duration: 0.5,
-})
-
-gsap.set('.paw--right', {
-  yPercent: 175,
-  xPercent: 15,
-})
-
-gsap.set('.number--next', {
-  yPercent: -100,
-  opacity: 0,
-})
-
-const RESIZE_TL = timeline()
-  .set('.transformer__feature', {
-    xPercent: -100,
+const RESIZE_TL = () =>
+  timeline({
+    delay: 1,
   })
-  .to(
-    '.transformer__outline',
-    {
-      scaleX: 0.6,
-      duration,
-    },
-    0
-  )
-  .to(
-    '.transformer__front',
-    {
-      x: TTEXT.offsetWidth / 2,
-      duration,
-    },
-    0
-  )
-  .to(
-    '.transformer__back',
-    {
-      x: -(TTEXT.offsetWidth / 2),
-      duration,
-    },
-    0
-  )
-  .to(
-    '.number.number--current.number--single',
-    {
-      yPercent: 100,
-      opacity: 0,
-      duration,
-    },
-    0
-  )
-  .to(
-    '.number.number--next.number--single',
-    {
-      yPercent: 0,
-      duration,
+    .set('.bear-paw', {
       opacity: 1,
-    },
-    0
-  )
+    })
+    .to(
+      '.transformer__heart',
+      {
+        opacity: 1,
+        duration,
+      },
+      0
+    )
+    .to(
+      '.paw--left',
+      {
+        opacity: 0,
+        duration,
+      },
+      0
+    )
+    .to(
+      '.transformer__feature',
+      {
+        xPercent: -100,
+        duration,
+      },
+      0
+    )
+    .to(
+      '.transformer__outline',
+      {
+        scaleX: 0.6,
+        duration,
+      },
+      0
+    )
+    .to(
+      '.transformer__front',
+      {
+        x: TTEXT.offsetWidth / 2,
+        duration,
+      },
+      0
+    )
+    .to(
+      '.transformer__back',
+      {
+        x: -(TTEXT.offsetWidth / 2),
+        duration,
+      },
+      0
+    )
+    .to(
+      '.number.number--current.number--single',
+      {
+        yPercent: 100,
+        opacity: 0,
+        duration,
+      },
+      0
+    )
+    .to(
+      '.number.number--next.number--single',
+      {
+        yPercent: 0,
+        duration,
+        opacity: 1,
+      },
+      0
+    )
 
-const TRANSFORMER_TL = timeline({
-  paused: true,
-})
-  .set('.transformer__feature', {
-    xPercent: 0,
+const SLAP_TL = () =>
+  gsap
+    .timeline({
+      onStart: () => {
+        gsap.set('.slap', {
+          display: 'block',
+        })
+      },
+      onComplete: () => {
+        gsap.set('.slap', {
+          display: 'none',
+        })
+        gsap.set('.slap__circle', { clearProps: 'all' })
+      },
+    })
+    .to('.slap__circle', {
+      strokeWidth: 0,
+      scale: 2.5,
+      duration: 0.15,
+      transformOrigin: '50% 50%',
+    })
+    .fromTo(
+      '.slap__line path',
+      {
+        strokeDashoffset: -11,
+      },
+      {
+        yPercent: -400,
+        duration: 0.15,
+        strokeDashoffset: 11,
+      },
+      0
+    )
+
+const CELEBRATE_TL = () =>
+  gsap
+    .timeline({
+      repeatRefresh: true,
+      onStart: () => {
+        gsap.set(['.wiggle', '.dashed__container'], {
+          display: 'block',
+        })
+      },
+      onComplete: () => {
+        gsap.set(['.wiggle', '.dashed__container'], {
+          display: 'none',
+        })
+      },
+    })
+    .set('.dashed__line', {
+      yPercent: -120,
+    })
+    .set('.dashed', {
+      yPercent: 0,
+    })
+    .set(['.wiggle', '.dashed__container'], {
+      rotate: () => Math.random() * 360,
+    })
+    .set(['.wiggle__line', '.dashed'], {
+      yPercent: () => Math.random() * 150 + 180,
+      scaleY: () => Math.random() + 0.5,
+    })
+    .fromTo(
+      '.dashed__line',
+      {
+        yPercent: -120,
+      },
+      {
+        yPercent: 120,
+        duration: 0.5,
+      },
+      0
+    )
+    .to('.dashed', {
+      yPercent: 'random(20, 100)',
+    })
+    .fromTo(
+      '.wiggle__line',
+      {
+        strokeDashoffset: -32,
+      },
+      {
+        yPercent: '+=50',
+        duration: 0.5,
+        strokeDashoffset: 32,
+      },
+      0
+    )
+
+const TRANSFORMER_TL = () =>
+  timeline({
+    onComplete: () => {
+      TRANSFORMER.removeAttribute('disabled')
+    },
   })
-  .set('.transformer__text', {
-    yPercent: 100,
-    opacity: 0,
-  })
-  .add(LEFT_PAW_TL)
-  .add(RIGHT_PAW_TL, 0.5)
-  .add(RESIZE_TL)
+    // NOTE:: Remove when not debugging
+    // .set('.transformer__feature', {
+    //   '--hovered': 1,
+    // })
+    // Set the hover states concretely
+    .set('.bear-paw', {
+      opacity: 1,
+    })
+    .set('.transformer__text', {
+      yPercent: 80,
+      opacity: 0,
+    })
+    // Make the left paw movement using the feature element
+    // This means the heart and the paw track movement
+    .add(LEFT_PAW_TL(), 0.25)
+    .add(RIGHT_PAW_TL(), '<')
+    .add(SLAP_TL(), '>-0.25')
+    .add(CELEBRATE_TL(), '<')
+    .add(RESIZE_TL())
 
 const RESET_TL = timeline({
   paused: true,
 })
-  .set(['.transformer__feature', '.transformer__text'], {
+  .set(['.transformer__feature', '.transformer__text', '.paw', '.bear-paw'], {
     clearProps: 'all',
   })
   .to('.transformer__front', {
@@ -154,7 +310,14 @@ TRANSFORMER.addEventListener('click', () => {
     TRANSFORMER.setAttribute('aria-pressed', false)
     RESET_TL.restart()
   } else {
-    TRANSFORMER_TL.restart()
     TRANSFORMER.setAttribute('aria-pressed', true)
+    TRANSFORMER.setAttribute('disabled', true)
+    // TRANSFORMER_TL.time(0)
+    // TRANSFORMER_TL.play()
+    TRANSFORMER_TL()
   }
 })
+
+// GSDevTools.create({
+//   animation: TRANSFORMER_TL,
+// })
