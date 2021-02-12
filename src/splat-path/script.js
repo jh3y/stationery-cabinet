7786fc49a96c9a899c591b5f032aef75cee3895f
jh3y <jh3y@users.noreply.github.com>
2021-02-12 00:03:57 +0000

    feat: squealy portraits

--- src/splat-path/script.js
+++ src/splat-path/script.js
@@ -1,1 +1,32 @@
-// 404
+import gsap from 'https://cdn.skypack.dev/gsap'
+
+const AUDIO = {
+  IN: new Audio('https://assets.codepen.io/605876/squeak-in.mp3'),
+  OUT: new Audio('https://assets.codepen.io/605876/squeak-out.mp3'),
+}
+
+const bindSound = splat => {
+  splat.addEventListener('pointerdown', () => {
+    AUDIO.OUT.pause()
+    AUDIO.IN.currentTime = AUDIO.OUT.currentTime = 0
+    AUDIO.IN.play()
+  })
+  splat.addEventListener('pointerup', () => {
+    AUDIO.IN.pause()
+    AUDIO.IN.currentTime = AUDIO.OUT.currentTime = 0
+    AUDIO.OUT.play()
+  })
+}
+
+const SPLATS = document.querySelectorAll('.splatted')
+SPLATS.forEach(splat => bindSound(splat))
+
+const LIMIT = 25
+document.addEventListener('pointermove', ({ x, y }) => {
+  const posX = gsap.utils.mapRange(0, window.innerWidth, LIMIT, -LIMIT, x)
+  const posY = gsap.utils.mapRange(0, window.innerHeight, LIMIT, -LIMIT, y)
+  gsap.set(document.documentElement, {
+    '--x': posX,
+    '--y': posY,
+  })
+})
