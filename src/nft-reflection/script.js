75257bde590877f59a15f32e07ca6df1e095c047
jh3y <jh3y@users.noreply.github.com>
2021-03-20 16:58:07 +0000

    feat: nft reflection

---
+++ src/nft-reflection/script.js
@@ -0,0 +1,21 @@
+// import { GUI } from 'https://cdn.skypack.dev/dat.gui'
+
+// const CONFIG = {
+//   rotateX: 0,
+//   rotateY: 0,
+// }
+
+// const CONTROLLER = new GUI()
+
+// const UPDATE = () => {
+//   for (const KEY of Object.keys(CONFIG)) {
+//     document.documentElement.style.setProperty(`--${KEY}`, CONFIG[KEY])
+//   }
+// }
+
+// CONTROLLER.add(CONFIG, 'rotateX', -360, 360, 1)
+//   .name('Rotate X')
+//   .onChange(UPDATE)
+// CONTROLLER.add(CONFIG, 'rotateY', -360, 360, 1)
+//   .name('Rotate Y')
+//   .onChange(UPDATE)
